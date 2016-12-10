8fe2ec6847d12d7c8d0d136971daba425b1f29d9
ymqui <ymqui@yahoo.com>
2016-12-10 17:00:40 -0500

    Add files via upload

--- info/index_birds_names.js
+++ info/index_birds_names.js
@@ -1,6 +1,6 @@
 ﻿<!--
   //date
-  var modiDate  = new Date("2016-12-05T16:00:00");
+  var modiDate  = new Date("2016-12-10T16:00:00");
   var modirange = 2;  	//range within modiDate in days
   var modiindx  = [0];	//index of which modibird to be shown first
   
@@ -581,7 +581,7 @@
 //birds[j++] = new myBird("   ","16-00-00T00:00","PACHYCEP","Crested Shriketit","鵙雀鹟","Falcunculus frontatus",[m_id("")],["mdbhrp","9/2015"],7143812);
   birds[j++] = new myBird("CHN","16-01-10T09:41","LANIIDAE","Long-tailed Shrike","棕背伯劳","Lanius schach",[p_id("1601china",43)],["cnlynv","1/2016"],56873);
   birds[j++] = new myBird("USA","16-02-05T09:01","LANIIDAE","Loggerhead Shrike","呆头伯劳","Lanius ludovicianus",[m_id("20160205_014_LoggerheadShrike")],["arbitr","2/2016","Dry Bridge Road, Frederick County, Maryland","干桥路，弗雷德里克郡，马里兰州"],4801026);
-//birds[j++] = new myBird("USA","16-00-00T00:00","LANIIDAE","Northern Shrike","灰伯劳","Lanius excubitor",[m_id("")],["arbitr","12/2016","Ben's Point Road, Queen Anne's County, Maryland","本点路，安妮女王郡，马里兰州"],71554);
+  birds[j++] = new myBird("USA","16-12-10T08:44","LANIIDAE","Northern Shrike","灰伯劳","Lanius excubitor",[m_id("20161210_138_NorthernShrike")],["arbitr","12/2016","Ben's Point Road, Queen Anne's County, Maryland","本点路，安妮女王郡，马里兰州"],71554);
   birds[j++] = new myBird("USA","15-09-13T07:30","VIREONID","White-eyed Vireo","白眼莺雀","Vireo griseus",[m_id("20160911_095_White-eyedVireo"),m_id("20150913_095_White-eyedVireo(I)")],["mdlbwt","9/2016","mdlbwt","9/2015","Immature"],4791453);
 //birds[j++] = new myBird("USA","16-00-00T00:00","VIREONID","Yellow-throated Vireo","黄喉莺雀","Vireo flavifrons",[p_id("asheville",)],["nccrsp","4/2016"],4754888);
   birds[j++] = new myBird("USA","16-04-23T08:15","VIREONID","Blue-headed Vireo","蓝头莺雀","Vireo solitarius",[m_id("20161106_114_Blue-headedVireo"),p_id("1609vtcanbirding",15)],["mdcrvp","11/2016","cactnw","9/2016"],4754897);
