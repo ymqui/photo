0506c4c613a0700255f241f99a9508c01c1522e6
ymqui <ymqui@yahoo.com>
2017-02-13 20:33:23 -0500

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -5,8 +5,8 @@ var dir        = maindir+"pics/qiuqiu/";
 var thumb_dir  = maindir+"thumbnail/";
 var interval   = 5000;
 var ext        = ".JPG";
-var midi_file  = "StrangeLandsPeople.mid";
-var midi_cr    = new Array("Mfiles","http://www.mfiles.co.uk/midi-files.htm");
+var midi_file  = "StoryOfMyLife.mid";
+var midi_cr    = new Array("Piano It All","http://www.pianoitall.com");
  
 if(window.usechinese){
     title      = "球球的照片";
@@ -19,14 +19,12 @@ function initAlbum(){
     this[0] = new Array();  //store file names
     this[1] = new Array();  //store file infos,shown as browser status
     i = 0;
-    this[0][i]   = "";
-    this[1][i++] = "1/29/2017 Ovulation test.";
-    this[0][i]   = "";
-    this[1][i++] = "2/14/2017 Pregnancy test.";
+    this[0][i]   = "20170128_001_OvulationTest";
+    this[1][i++] = "1/28/2017 Ovulation test.";
     if(window.usechinese){
         i = 0;
-        this[1][i++] = "1/29/2017 排卵测试。";
-        this[1][i++] = "2/14/2017 怀孕测试。";
+        this[1][i++] = "1/28/2017 排卵测试。";
+        //this[1][i++] = "2/14/2017 怀孕测试。";
     }
 }
  
