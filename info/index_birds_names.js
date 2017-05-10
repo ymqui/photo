7fe00107093d211ff0ab10c8dab13d556b35bd82
ymqui <ymqui@yahoo.com>
2017-05-10 14:22:27 -0400

    Add files via upload

--- info/index_birds_names.js
+++ info/index_birds_names.js
@@ -84,6 +84,8 @@
   locs["nyjbsp"] = ["Jones Beach State Park","New York","琼斯海滩州立公园","纽约州","http://nysparks.com/parks/JonesBeach/"];
   locs["nyjbwr"] = ["Jamaica Bay Wildlife Refuge","New York","牙买加湾野生动物保护区","纽约州","http://www.nyharborparks.org/visit/jaba.html"];
   locs["nymnwr"] = ["Montezuma National Wildlife Refuge","New York","蒙特祖玛国家野生动物保护区","纽约州","http://www.fws.gov/refuge/montezuma/"];
+//locs["ohcvnp"] = ["Cuyahoga Valley National Park","Ohio","库雅荷加谷国家公园","俄亥俄州","https://www.nps.gov/cuva/",baikeurl("库雅荷加谷国家公园")];
+//locs["ohment"] = ["Mentor Marsh State Nature Perserve","Ohio","门托沼泽州自然保护区","俄亥俄州","http://naturepreserves.ohiodnr.gov/mentormarsh"];
   locs["orcanb"] = ["Cannon Beach","Oregon","卡农海滩","俄勒冈州","http://www.ci.cannon-beach.or.us/~Natural/HRAP/hrap-habitat.html"];
   locs["orhayd"] = ["Hayden Island","Portland, Oregon","波特兰海登岛","俄勒冈州",wikiurl("Hayden Island, Portland, Oregon")];
   locs["prbaya"] = ["Bayamón","Puerto Rico","巴亚蒙","波多黎各",wikiurl("Bayamón, Puerto Rico")];
