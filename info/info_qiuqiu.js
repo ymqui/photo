205c69293382c9ab5f003f883d8732ad66185021
ymqui <ymqui@yahoo.com>
2017-06-03 19:48:03 -0400

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -27,14 +27,14 @@ function initAlbum(){
     this[0][i]   = "20170309_003_Sonogram";
     this[1][i++] = "Sonogram at MedStar Montgomery Medical Center. Our greatest joy to hear the baby's heartbeat for the first time, 3/9/2017.";
     this[0][i]   = "20170602_007_AnatomySonogram";
-    this[1][i++] = "Anatomy ultrasoud at MedStar Montgomery Medical Center. Everything is fine with the baby, 6/2/2017.";
+    this[1][i++] = "Anatomy ultrasoud at MedStar Montgomery Medical Center. The baby is healthy and strong, 6/2/2017.";
 
     if(window.usechinese){
         i = 0;
         this[1][i++] = "排卵测试显阳性，1/28/2017。";
         this[1][i++] = "怀孕测试显阳性，2/15/2017。";
         this[1][i++] = "在蒙哥马利医疗中心做超声波检查。无比幸福地看到和听到胎儿的心跳，3/9/2017。";
-        this[1][i++] = "在蒙哥马利医疗中心做解剖结构超声波检查，胎儿一切都正常，6/2/2017。";
+        this[1][i++] = "在蒙哥马利医疗中心做解剖结构超声波检查，胎儿很健康，6/2/2017。";
     }
 }
  
