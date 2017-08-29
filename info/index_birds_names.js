37d56beeb591ad60e5fd76abd2fa1568cdf5f7d6
ymqui <ymqui@yahoo.com>
2017-08-29 11:16:53 -0400

    Add files via upload

--- info/index_birds_names.js
+++ info/index_birds_names.js
@@ -40,9 +40,9 @@
   locs["gatybe"] = ["Tybee Island","Georgia","泰碧岛","乔治亚州","https://tybeeisland.com/tybee-information/birding-nature-trails/"];
   locs["macast"] = ["Castle Island","Boston, Massachusetts","波士顿城堡岛","马萨诸塞州",wikiurl("Castle Island (Massachusetts)")];
   locs["mahpsp"] = ["Halibut Point State Park","Rockport, Massachusetts","大比目鱼角州立公园","马萨诸塞州","http://www.mass.gov/eea/agencies/dcr/massparks/region-north/halibut-point-state-park.html"];
-  locs["mdbhrp"] = ["Black Hill Regional Park","Maryland","黑山公园","马里兰州","http://www.montgomeryparks.org/facilities/regional_parks/blackhill/"];
-  locs["mdbmnt"] = ["Blue Mash Nature Trail","Maryland","蓝沼自然小径","马里兰州","http://www.montgomeryparks.org/PPSD/ParkTrails/trails_MAPS/blue_mash.shtm"];
-  locs["mdbrgd"] = ["Brookside Garden","Wheaton, Maryland","惠顿溪边花园","马里兰州","http://www.montgomeryparks.org/brookside/"];
+  locs["mdbhrp"] = ["Black Hill Regional Park","Maryland","黑山公园","马里兰州","http://www.montgomeryparks.org/parks-and-trails/black-hill-regional-park/"];
+  locs["mdbmnt"] = ["Blue Mash Nature Trail","Maryland","蓝沼自然小径","马里兰州","http://old.montgomeryparks.org/PPSD/ParkTrails/trails_MAPS/blue_mash.shtm"];
+  locs["mdbrgd"] = ["Brookside Garden","Wheaton, Maryland","惠顿溪边花园","马里兰州","http://www.montgomeryparks.org/parks-and-trails/brookside-gardens/"];
   locs["mdcamb"] = ["Cambridge","Maryland","剑桥","马里兰州",wikiurl("Cambridge, Maryland")];
   locs["mdcapc"] = ["Congressional Polo Club","Poolesville, Maryland","普尔斯维尔镇国会马球俱乐部","马里兰州","http://www.congressionalpolo.com/"];
   locs["mdcocn"] = ["C&O Canal","Maryland","C&O运河","马里兰州","http://www.nps.gov/choh/",baikeurl(1767125)];
@@ -56,7 +56,7 @@
   locs["mdgrpt"] = ["Greenbury Point","Annapolis, Maryland","安纳波利斯格林伯瑞角","马里兰州","http://www.fws.gov/chesapeakebay/greenpt.html"];
   locs["mdhadg"] = ["Havre de Grace","Maryland","哈佛德格雷斯","马里兰州",wikiurl("Havre de Grace, Maryland")];
   locs["mdinhb"] = ["Baltimore Harbor","Maryland","巴尔的摩港","马里兰州",wikiurl("Inner Harbor"),baikeurl(48720)];
-  locs["mdlbrp"] = ["Little Bennett Regional Park","Maryland","小班尼特公园","马里兰州","http://www.montgomeryparks.org/facilities/regional_parks/little_bennett/"];
+  locs["mdlbrp"] = ["Little Bennett Regional Park","Maryland","小班尼特公园","马里兰州",http://www.montgomeryparks.org/parks-and-trails/little-bennett-regional-park/];
   locs["mdloch"] = ["Loch Raven Reservoir","Maryland","渡鸦湖水库","马里兰州",wikiurl("Loch Raven Reservoir")];
   locs["mdmapp"] = ["Mariner Point Park","Harford County, Maryland","哈福德郡水手角公园","马里兰州","http://www.harfordcountymd.gov/Facilities/Facility/Details/Mariner-Point-Park-69"];
   locs["mdmwma"] = ["McKee-Beshers Wildlife Management Area","Maryland","McKee-Beshers野生动物管理区","马里兰州","http://dnr.maryland.gov/wildlife/Pages/publiclands/central/mckeebeshers.aspx"];
