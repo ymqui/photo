a863bc188aefa1fbbf5ff84543c6bc886dfb72ab
ymqui <ymqui@yahoo.com>
2017-10-09 13:52:47 -0400

    Add files via upload

---
+++ info/info_1709babyshower.js
@@ -0,0 +1,39 @@
+﻿<!--
+//Created by Album Builder Program Mon Oct 09 13:51:08 2017
+var info_url   = "";
+var title      = "Baby Shower";
+var cam_img    = "camera_canon_6d.jpg";
+var cam_tit    = "Canon SLR EOS 6D, 20.2 Mega Pixels.";
+var dir        = maindir+"pics/1709babyshower/";
+var thumb_dir  = maindir+"thumbnail/";
+var interval   = 5000;
+var ext        = ".jpg";
+var midi_file  = "";
+//var midi_cr    = new Array("","");
+ 
+if(window.usechinese){
+    title      = "迎婴聚会";
+    cam_tit    = "佳能单镜头反光式6D相机，2020万像素。";
+}
+ 
+document.title = title;
+ 
+function initAlbum(){
+    this.length = 2;
+    this[0] = new Array();  //store file names
+    this[1] = new Array();  //store file infos,shown as browser status
+    for (var i=0;i<40;i++){
+        this[0][i] = "1709babyshower_"+(i+1).toString();
+        this[1][i] = "";
+    }
+    if(window.usechinese){
+    }else{
+    }
+}
+ 
+//web counter info
+var sc_project   = 613905;
+var sc_invisible = 1;
+var sc_partition = 3;
+var sc_security  = "f5cfdd4f";
+//-->
