6ba61d616e587371ddd38586cb04102e2ae556c9
ymqui <ymqui@yahoo.com>
2017-12-06 11:43:16 -0500

    Add files via upload

--- script/copyright_birds.js
+++ script/copyright_birds.js
@@ -67,6 +67,5 @@
   var sc_partition=13; 
   var sc_security="382e0c58"; 
   var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
-  document.write('<br>TEST');
-  document.write('<script type="text/javascript" language="javascript" async src="'+scJsHost+'aaastatcounter.com/counter/counter.js"></script>'); window.focus();
+  document.write('<script type="text/javascript" language="javascript" async src="'+scJsHost+'statcounter.com/counter/counter.js"></script>'); window.focus();
 //-->
