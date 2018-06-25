e57d0b50b0d3252842ac968e6d497fcf69b754c9
ymqui <ymqui@yahoo.com>
2018-06-25 12:05:46 -0400

    Add files via upload

--- script/album.js
+++ script/album.js
@@ -586,4 +586,14 @@ function switchlang(){
 function pagelink(pid, descrip){
     return "<a href='showphoto.html?"+pid+"' style='color: #3399FF; text-decoration: underline;' target='_blank'>"+descrip+"</a>";
 } 
+
+function getdate(name){
+    var year = name.substring(0,4);
+    var mon  = name.substring(4,6);
+    var day  = name.substring(6,8);
+    if (mon.substring(0,1)=="0"){mon = mon.substring(1,2);}
+    if (day.substring(0,1)=="0"){day = day.substring(1,2);}
+    if (window.usechinese) {return "，"+mon+"/"+day+"/"+year+"。";}
+    else{return ", "+mon+"/"+day+"/"+year+"." ;}
+}
 //-->
