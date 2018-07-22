fc399e0e712f9c6e2bcb15f3b86ecc055a926b95
ymqui <ymqui@yahoo.com>
2018-07-22 17:17:24 -0400

    Add files via upload

--- script/body.js
+++ script/body.js
@@ -165,7 +165,7 @@
   if (window.pagelist){
      for (var i=0;i<3;i++) document.write('<p>&nbsp;<p style="font-size: '+fsize+' color:'+color_3+'; vertical-align:middle;">');
      document.write('<img src="'+thumb_dir+'branch_l.png">');
-     if (window.usechinese){document.write('相关照片');}else{document.write('Related Photos');}
+     if (window.usechinese){document.write(' 相关照片 ');}else{document.write(' Related Photos ');}
      document.write('<img src="'+thumb_dir+'branch_r.png">');
      document.write('<p style="margin-top:6px; font-size: '+fsize+'">');
      for (var i=pagelist.length-1;i>=0;i--){
