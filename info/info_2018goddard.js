7b0ccd4ac42cb9c153a36f6a3006f787cb646ec0
ymqui <ymqui@yahoo.com>
2018-09-24 16:01:31 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -156,6 +156,8 @@ function initAlbum(){
     this[1][i++]  = (["Group painting","集体绘画"])[cn_ind];
     this[0][i]    = "20180924_1";
     this[1][i++]  = (["Little firefighter","小小消防员"])[cn_ind];
+    this[0][i]    = "20180924_2";
+    this[1][i++]  = (["Playing with red paint","涂手画画"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = this[1][i]+getdate(this[0][i]);
