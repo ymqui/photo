edc74501e5e9285e74d5feae8d2e933c636379c3
ymqui <ymqui@yahoo.com>
2018-10-01 14:20:52 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -164,6 +164,8 @@ function initAlbum(){
     this[1][i++]  = (["Classmate's birthday celebration","同学的生日"])[cn_ind];
     this[0][i]    = "20180926_2";
     this[1][i++]  = (["Enjoying the sippy cup","用吸管杯喝奶"])[cn_ind];
+    this[0][i]    = "20181001_1";
+    this[1][i++]  = (["Crawling towards his favorite toy","爬着去抓玩具"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = this[1][i]+getdate(this[0][i]);
