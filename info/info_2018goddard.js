c5efc4883016c14b1f54b88093c8297cc1901655
ymqui <ymqui@yahoo.com>
2018-10-31 16:37:38 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -210,6 +210,8 @@ function initAlbum(){
     this[1][i++]  = (["Happy Halloween and Happy Birthday","生日快乐，万圣节快乐"])[cn_ind];
     this[0][i]    = "20181031_4";
     this[1][i++]  = (["Exploring the tunnel","和同学一起玩"])[cn_ind];
+    this[0][i]    = "20181031_5";
+    this[1][i++]  = (["Happy Birthday","生日快乐"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = this[1][i]+getdate(this[0][i]);
