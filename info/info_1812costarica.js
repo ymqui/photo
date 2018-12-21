999c75810ce18def9c3a00edd2c6e81e4b66ca37
ymqui <ymqui@yahoo.com>
2018-12-21 11:11:03 -0500

    Add files via upload

--- info/info_1812costarica.js
+++ info/info_1812costarica.js
@@ -10,6 +10,7 @@ var interval   = 5000;
 var ext        = ".JPG";
 var midi_file  = "";
 //var midi_cr    = new Array("","");
+var pagelist   = [["1812crbirding","12/2018 Birding in Costa Rica","12/2018 哥斯达黎加观鸟"]]
  
 if(window.usechinese){
     title      = "游哥斯达黎加";
