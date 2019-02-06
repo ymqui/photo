de55add21a9f9e44812b4b521b69ad0fc6af7897
ymqui <ymqui@yahoo.com>
2019-02-06 11:08:13 -0500

    Add files via upload

--- info/index_birds_names.js
+++ info/index_birds_names.js
@@ -38,7 +38,7 @@
       flmnwr:  ["Merritt Island National Wildlife Refuge","Florida, USA","美丽特岛国家野生动物保护区","美国佛罗里达州",npsurl("Merritt_Island",true)],
       flplpr:  ["Plantation Preserve","Broward County, Florida, USA","布劳沃德郡种植园保留地","美国佛罗里达州","http://www.plantation.org/Golf/Plantation-Preserve/linear-park.html"],
 //    gbhydp:  ["Hyde Park","London, United Kingdom","海德公园","英国伦敦",wikiurl("Hyde Park, London"),baikeurl("海德公园")],
-//    gblwcn:  ["London Wetland Centre","United Kingdom","伦敦湿地中心","伦敦","https://www.wwt.org.uk/wetland-centres/london/"],
+//    gblwcn:  ["London Wetland Centre","United Kingdom","伦敦湿地中心","英国","https://www.wwt.org.uk/wetland-centres/london/"],
       mahpsp:  ["Halibut Point State Park","Rockport, Massachusetts, USA","大比目鱼角州立公园","美国马萨诸塞州","http://www.mass.gov/eea/agencies/dcr/massparks/region-north/halibut-point-state-park.html"],
       mdbhrp:  ["Black Hill Regional Park","Maryland, USA","黑山公园","美国马里兰州","http://www.montgomeryparks.org/parks-and-trails/black-hill-regional-park/"],
       mdbmnt:  ["Blue Mash Nature Trail","Maryland, USA","蓝沼自然小径","美国马里兰州","http://old.montgomeryparks.org/PPSD/ParkTrails/trails_MAPS/blue_mash.shtm"],
