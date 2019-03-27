7ffbd01aa8b05547a383114292a5b344af433649
ymqui <ymqui@yahoo.com>
2019-03-27 09:26:45 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -324,7 +324,7 @@ function initAlbum(){
     this[0][i]    = "20190320_1";
     this[1][i++]  = (["The first day of spring","春天的第一天"])[cn_ind];
     this[0][i]    = "20190322_1";
-    this[1][i++]  = (["Painting with bare hands","徒手画画"])[cn_ind];
+    this[1][i++]  = (["Painting on aluminum foil with bare hands","徒手在铝箔上画画"])[cn_ind];
     this[0][i]    = "20190326_1";
     this[1][i++]  = (["Outdoor time","户外活动"])[cn_ind];
 
