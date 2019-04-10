3b83738fdb4f6f7080800f54d4d1a62a902ccffe
ymqui <ymqui@yahoo.com>
2019-04-10 10:14:43 -0400

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -147,7 +147,7 @@ function initAlbum(){
     this[0][i]    = "20190220_Reading";
     this[1][i++]  = (["Reading with Mommy","和妈妈一起看书"])[cn_ind];
     this[0][i]    = "20190407_008";
-    this[1][i++]  = (["Reading with James and Uncle He","和James哥哥一起坐在小何叔叔腿上看书"])[cn_ind];
+    this[1][i++]  = (["Reading with James and Uncle He","和James哥哥一起坐在小何叔叔怀里看书"])[cn_ind];
     this[0][i]    = "20190407_009";
     this[1][i++]  = (["Playing with sister Adele","和小希姐姐一块拼积木"])[cn_ind];
 
