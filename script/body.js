181a140061e8ce0d854c8c0c5f2303edddace77a
ymqui <ymqui@yahoo.com>
2019-04-23 16:05:30 -0400

    Add files via upload

--- script/body.js
+++ script/body.js
@@ -171,7 +171,7 @@
      document.write('<p style="margin-top:6px; font-size: '+fsize+'">');
      for (var i=0;i<pagelist.length;i++){
          document.write(pagelink(pagelist[pagelist.length-1-i][0],pagelist[pagelist.length-1-i][pagelistid]));
-         if ((i!=(pagelist.length-1))&&(!cellphone)){document.write(',');}
+         if ((i!=(pagelist.length-1))&&(!cellphone)){document.write('<font color='+color_3+'>,</font>');}
          document.write('&nbsp;');
          if (i%3==2||cellphone){document.write('<br>');}
      }  
