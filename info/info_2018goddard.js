a5d8161230fbfc4ce7d64e54bac2e75182fb8a67
ymqui <ymqui@yahoo.com>
2019-05-20 10:40:01 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -383,6 +383,12 @@ function initAlbum(){
     this[1][i++]  = (["Goal of painting: making a mess","徒手画画"])[cn_ind];
     this[0][i]    = "20190507_1";
     this[1][i++]  = (["Playing with paper","玩纸片"])[cn_ind];
+    this[0][i]    = "20190508_1";
+    this[1][i++]  = (["Painting with a cookie cutter","用饼干模具绘画"])[cn_ind];
+    this[0][i]    = "20190508_2";
+    this[1][i++]  = (["Playing in the playground","户外活动"])[cn_ind];
+    this[0][i]    = "20190508_3";
+    this[1][i++]  = (["Working on the Mother's Day project","准备母亲节礼物"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
