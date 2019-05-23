9c27fe2a46076895ea4f17ee636702995afa67bd
ymqui <ymqui@yahoo.com>
2019-05-22 20:47:40 -0400

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -148,9 +148,9 @@ function initAlbum(){
     this[1][i++]  = (["Playing with sister Adele","和小希姐姐一块拼积木"])[cn_ind];
     this[0][i]    = "20190420_022";
     this[1][i++]  = ([pagelink("1904dcmuseum","Visiting the Smithonian museums")+" in Washington DC","去"+pagelink("1904dcmuseum","华盛顿特区参观博物馆")])[cn_ind];
-    this[0][i]    = "20190511_012_Pyramid";
-    this[1][i++]  = (["Visting the Teotihuacan pyramids during the vacation in Mexico","在墨西哥度假期间参观特奥蒂瓦坎古城金字塔"])[cn_ind];
-//  this[1][i++]  = (["Visting the Teotihuacan pyramids during the "+pagelink("1905mexico","vacation in Mexico"),"在"+pagelink("1905mexico","墨西哥度假")+"期间参观特奥蒂瓦坎古城金字塔"])[cn_ind];
+    this[0][i]    = "20190516_061_Zocalo";
+    this[1][i++]  = (["Visiting the Zocalo in Mexico City during the vacation in Mexico","在墨西哥度假期间逛墨西哥城的宪法广场"])[cn_ind];
+//  this[1][i++]  = (["Visiting the Teotihuacan pyramids during the "+pagelink("1905mexico","vacation in Mexico"),"在"+pagelink("1905mexico","墨西哥度假")+"期间参观特奥蒂瓦坎古城金字塔"])[cn_ind];
 
 
     for (var i=0;i<this[0].length;i++){
