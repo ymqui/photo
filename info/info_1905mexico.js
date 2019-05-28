2682ad66dce1d74f390865dc6b00b6091f4301cf
ymqui <ymqui@yahoo.com>
2019-05-28 06:42:50 -0400

    Add files via upload

--- info/info_1905mexico.js
+++ info/info_1905mexico.js
@@ -49,7 +49,7 @@ function initAlbum(){
     this[1][i++] = (["In the Jardin de la Union in Guanajuato City.","在瓜纳华托城的联盟花园。"])[cn_ind];
     this[1][i++] = (["Wandering the alleys in Guanajuato City.","逛瓜纳华托城的小巷。"])[cn_ind];
     this[1][i++] = (["Wandering the alleys in Guanajuato City.","逛瓜纳华托城的小巷。"])[cn_ind];
-    this[1][i++] = (["Inside an art gallery in Guanajuato City.","在瓜纳华托城的一个画廊里欣赏图片。"])[cn_ind];
+    this[1][i++] = (["Inside an art gallery in Guanajuato City. The two mojigangas characters are famous artist Diego Rivera and Frida Kahlo.","在瓜纳华托城的一个画廊里欣赏图片。背后的两个人偶是墨西哥著名画家迭戈·里维拉和弗里达·卡罗夫妇。"])[cn_ind];
     this[1][i++] = (["A wall sculpture in Guanajuato City.","墙上的名人纪念雕塑。"])[cn_ind];
     this[1][i++] = (["Inside a crafts market in Guanajuato City.","瓜纳华托城的一个艺术品市场。"])[cn_ind];
     this[1][i++] = (["Wandering the alleys in Guanajuato City.","逛瓜纳华托城的小巷。"])[cn_ind];
