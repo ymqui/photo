d91757a3fe51aec23070b26cdba5d1afcb7c90d9
ymqui <ymqui@yahoo.com>
2019-06-10 11:12:58 -0400

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -150,7 +150,10 @@ function initAlbum(){
     this[1][i++]  = ([pagelink("1904dcmuseum","Visiting the Smithonian museums")+" in Washington DC","去"+pagelink("1904dcmuseum","华盛顿特区参观博物馆")])[cn_ind];
     this[0][i]    = "20190516_061_Zocalo";
     this[1][i++]  = (["Visiting the Zocalo in Mexico City during the "+pagelink("1905mexico","vacation in Mexico"),"在"+pagelink("1905mexico","墨西哥度假")+"期间逛墨西哥城中心的宪法广场"])[cn_ind];
-
+    this[0][i]    = "20190608_031MiniTrain";
+    this[1][i++]  = ([pagelink("1906minitrain","Riding the miniature train")+" in Wheaton Regional Park","在"+pagelink("1906minitrain","惠顿公园坐小火车")])[cn_ind];
+    this[0][i]    = "20190609_040";
+    this[1][i++]  = (["Writing in the feeding diary","喂养日记"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
