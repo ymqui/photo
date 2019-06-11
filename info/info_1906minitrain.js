5c7db3bebdc0be4f209c46880588412a0374a8d7
ymqui <ymqui@yahoo.com>
2019-06-11 05:39:23 -0400

    Add files via upload

--- info/info_1906minitrain.js
+++ info/info_1906minitrain.js
@@ -23,7 +23,7 @@ function initAlbum(){
     this.length = 2;
     this[0] = new Array();  //store file names
     this[1] = new Array();  //store file infos,shown as browser status
-    for (var i=0;i<9;i++){
+    for (var i=0;i<8;i++){
         this[0][i] = "1906minitrain_"+(i+1).toString();
         this[1][i] = "";
     }
