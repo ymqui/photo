2aca969aa46ede579bb3ca8deef08c80824d65c6
ymqui <ymqui@yahoo.com>
2019-07-01 11:21:32 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -435,6 +435,8 @@ function initAlbum(){
     this[1][i++]  = (["Reading time","读书"])[cn_ind];
     this[0][i]    = "20190628_1";
     this[1][i++]  = (["Pointing out where the nose is","跟老师指出鼻子在哪里"])[cn_ind];
+    this[0][i]    = "20190701_1";
+    this[1][i++]  = (["Out for a walk","户外活动"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
