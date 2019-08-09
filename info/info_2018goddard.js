61855ff5b912f728f379a23059fb0d21b0f3a0c0
ymqui <ymqui@yahoo.com>
2019-08-09 10:11:04 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -488,7 +488,7 @@ function initAlbum(){
     this[0][i]    = "20190806_1";
     this[1][i++]  = (["Mixing red paint and yellow paint to make orange","混合红色和黄色颜料调配出橙色"])[cn_ind];
     this[0][i]    = "20190808_1";
-    this[1][i++]  = (["Singing the Choo Choo Train song","大家一起唱Choo Choo火车歌"])[cn_ind];
+    this[1][i++]  = (["Singing the Choo Choo Train song","Luczon老师带大家一起唱火车呜呜歌"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
