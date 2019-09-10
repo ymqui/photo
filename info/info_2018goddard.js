1b08d06e45a83f25293f69c53db79519fa3d1906
ymqui <ymqui@yahoo.com>
2019-09-09 20:08:10 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -1,4 +1,4 @@
-﻿<!--
+<!--
 var cn_ind     = window.usechinese?1:0;
 var info_url   = "https://www.goddardschool.com/washington/gaithersburg-wind-river-lane-md";
 var title      = (["Nathan at Goddard School","球球在戈达德托儿所的照片"])[cn_ind];
@@ -517,6 +517,8 @@ function initAlbum(){
     this[1][i++]  = (["Painting on the first day in the new classroom","在新教室的第一天"])[cn_ind];
     this[0][i]    = "20190909_1";
     this[1][i++]  = (["Nathan loves painting","喜爱画画的球球"])[cn_ind];
+    this[0][i]    = "20190909_2";
+    this[1][i++]  = (["Dancing and making music with instruments","唱歌跳舞演奏音乐"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
