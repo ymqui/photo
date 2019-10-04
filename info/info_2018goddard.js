ab71aab887bfa5be68941a5790fef7f36f76c3ac
ymqui <ymqui@yahoo.com>
2019-10-03 20:08:24 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -1,4 +1,4 @@
-﻿<!--
+<!--
 var cn_ind     = window.usechinese?1:0;
 var info_url   = "https://www.goddardschool.com/washington/gaithersburg-wind-river-lane-md";
 var title      = (["Nathan at Goddard School","球球在戈达德托儿所的照片"])[cn_ind];
@@ -561,6 +561,10 @@ function initAlbum(){
     this[1][i++]  = (["Using fingers to paint with different fall colors","用手指画秋天不同的色彩"])[cn_ind];
     this[0][i]    = "20191002_1";
     this[1][i++]  = (["Using an apple to paint with different fall colors","用苹果画秋天不同的色彩"])[cn_ind];
+    this[0][i]    = "20191003_1";
+    this[1][i++]  = (["First yoga class","在健身房上第一堂瑜伽课"])[cn_ind];
+    this[0][i]    = "20191003_2";
+    this[1][i++]  = (["First yoga class","在健身房上第一堂瑜伽课"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
