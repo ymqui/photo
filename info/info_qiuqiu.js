4c2c7b3ee92c90f9a6beb5fdda919af35b7b226f
ymqui <ymqui@yahoo.com>
2020-01-02 09:06:54 -0500

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -1,4 +1,4 @@
-<!--
+﻿<!--
 var cn_ind     = window.usechinese?1:0;
 var info_url   = "";
 var title      = (["Nathan's Photos","球球的照片"])[cn_ind];
@@ -14,7 +14,7 @@ var pagelist   = [["1806myrtlebeach","6/2018 Myrtle Beach, South Carolina","6/20
                   ["1812costarica","12/2018 Trip to Costa Rica","12/2018 游哥斯达黎加"],["1904dcmuseum","4/2019 Visiting the Smithonian Museums","4/2019 华盛顿特区参观博物馆"],
                   ["1905mexico","5/2019 Vacationing in Mexico","5/2019 墨西哥度假"],["1906minitrain","6/2019 Wheaton Miniature Train","6/2019 惠顿公园坐小火车"],
                   ["1906artechouse","6/2019 Infinite Space at Artechouse","6/2019 Artechouse无限空间展览"],["1908florida","8/2019 Vacationing in Florida","8/2019 佛罗里达度假"],
-                  ["1910parade","10/2019 Goddard School Halloween Parade","10/2019 戈达德托儿所万圣节游行"],["2018goddard","2018-2019 Goddard School","2018-2019 戈达德托儿所"]];
+                  ["1910parade","10/2019 Goddard School Halloween Parade","10/2019 戈达德托儿所万圣节游行"],["2018goddard","2018-2020 Goddard School","2018-2020 戈达德托儿所"]];
 var thisyear   = (new Date()).getFullYear();
 if (thisyear>2026) {pagelist = pagelist.concat([["world","2018-"+thisyear.toString()+" Travel Around the World","2018-"+thisyear.toString()+" 环游世界"]]);} 
 
