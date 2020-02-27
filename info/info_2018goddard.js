108944c052a59d1f8559292091f619f09c7e109f
ymqui <ymqui@yahoo.com>
2020-02-27 14:33:43 -0500

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -1,4 +1,4 @@
-<!--
+﻿<!--
 var cn_ind     = window.usechinese?1:0;
 var info_url   = "https://www.goddardschool.com/washington/gaithersburg-wind-river-lane-md";
 var title      = (["Nathan at Goddard School","球球在戈达德托儿所的照片"])[cn_ind];
@@ -804,6 +804,8 @@ function initAlbum(){
     this[1][i++]  = (["Brushing Bruno the dinosaur's teeth","给恐龙布鲁诺刷牙"])[cn_ind];
     this[0][i]    = "20200226_3";
     this[1][i++]  = (["Painting with a pompom","用绒球画画"])[cn_ind];
+    this[0][i]    = "20200227_1";
+    this[1][i++]  = (["Painting","画画"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
