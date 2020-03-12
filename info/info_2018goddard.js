e95bb801818bdcb286304ccc0c989f00c99d5567
ymqui <ymqui@yahoo.com>
2020-03-12 12:51:55 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -831,7 +831,9 @@ function initAlbum(){
     this[0][i]    = "20200310_2";
     this[1][i++]  = (["Taste-testing a piece of cheese","品尝奶酪"])[cn_ind];
     this[0][i]    = "20200311_1";
-    this[1][i++]  = (["Playing with rice","粘大米"])[cn_ind];
+    this[1][i++]  = (["Sprinkling rice to be glued to the paper","粘大米"])[cn_ind];
+    this[0][i]    = "20200312_1";
+    this[1][i++]  = (["Tasting milk","品尝牛奶"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
