cb5cbe2052c94e1559a0105c29f255ff67c79101
ymqui <ymqui@yahoo.com>
2020-04-03 09:14:58 -0400

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -177,7 +177,7 @@ function initAlbum(){
     this[0][i]    = ["2002trainshow/2002trainshow_18","20200209"];
     this[1][i++]  = (["Riding the handcar in the "+pagelink("2002trainshow","model train show")+" at Dulles Expo Center in Virginia","在弗吉尼亚州杜勒斯展览中心"+pagelink("2002trainshow","火车模型展")+"上坐手动小火车"])[cn_ind];
     this[0][i]    = ["2018goddard/20200403_1","20200403"];
-    this[1][i++]  = (["Distance learning from home due to the Coronavirus pandemic","托儿所由于全球2019冠状病毒疫情蔓延而关门，在家远程上课"])[cn_ind];
+    this[1][i++]  = (["Distance learning from home due to the Coronavirus pandemic",daycare+"由于全球2019冠状病毒疫情蔓延而关门，在家远程上课"])[cn_ind];
     
     for (var i=0;i<this[0].length;i++){
         if (this[0][i].constructor != Array){ 
