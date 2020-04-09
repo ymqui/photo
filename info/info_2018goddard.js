4c9e1867076ae8519352e5ad42b0a167e8c7877b
ymqui <ymqui@yahoo.com>
2020-04-09 10:19:07 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -882,6 +882,8 @@ function initAlbum(){
 //    this[1][i++]  = (["Playing with dominos","玩多米诺骨牌"])[cn_ind];
 //    this[0][i]    = "20200409_3";
 //    this[1][i++]  = (["Scooter ride in the gym","在健身房骑板车"])[cn_ind];
+//    this[0][i]    = "20200409_4";
+//    this[1][i++]  = (["Snack time","吃零食"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
