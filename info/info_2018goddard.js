f2e1ab41bddb2516a222fcfc193e5382c59bf3c3
ymqui <ymqui@yahoo.com>
2020-04-13 11:05:11 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -1,4 +1,4 @@
-﻿<!--
+<!--
 var cn_ind     = window.usechinese?1:0;
 var info_url   = "https://www.goddardschool.com/washington/gaithersburg-wind-river-lane-md";
 var title      = (["Nathan at Goddard School","球球在戈达德托儿所的照片"])[cn_ind];
@@ -886,6 +886,8 @@ function initAlbum(){
 //    this[1][i++]  = (["Painting","画画"])[cn_ind];
 //    this[0][i]    = "20200409_6";
 //    this[1][i++]  = (["Outdoor activity","户外活动"])[cn_ind];
+    this[0][i]    = "20200413_1";
+    this[1][i++]  = (["Distance learning at home","在家远程上课"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
