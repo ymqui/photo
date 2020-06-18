5b518553f6a86ca392d66fd4a94939a7337c13ae
ymqui <ymqui@yahoo.com>
2020-06-18 12:27:36 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -925,9 +925,11 @@ function initAlbum(){
     this[0][i]    = "20200616_1";
     this[1][i++]  = (["Painting with watercolor","画水彩画"])[cn_ind];
     this[0][i]    = "20200618_1";
-    this[1][i++]  = (["Class photo with Ms Bailey and Ms Dave","Bailey老师和Dave老师带大家合影"])[cn_ind];
+    this[1][i++]  = (["Class photo with Ms Bailey and Ms Dave","这个学期的最后一天Bailey老师和Dave老师带大家合影"])[cn_ind];
     this[0][i]    = "20200618_2";
-    this[1][i++]  = (["Photo with Ms Dave and Ms Bailey","和Bailey老师和Dave老师合影"])[cn_ind];
+    this[1][i++]  = (["Photo with Ms Dave and Ms Bailey","这个学期的最后一天和Bailey老师和Dave老师合影"])[cn_ind];
+    this[0][i]    = "20200618_3";
+    this[1][i++]  = (["Last day of school with the squirrel class and Ms. Burke and Ms. Aya","这个学期的最后一天松鼠教室和Burke老师和Aya老师合影"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
