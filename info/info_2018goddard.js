e0182cd7ee2d89aa5151b7498603afcc91cc5926
ymqui <ymqui@yahoo.com>
2020-07-08 13:14:38 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -947,7 +947,7 @@ function initAlbum(){
     this[0][i]    = "20200706_1";
     this[1][i++]  = (["Jumping in the puddle","在积水里跳"])[cn_ind];
     this[0][i]    = "20200706_2";
-    this[1][i++]  = (["Putting felt pieces in crazy straw","将布片丳在吸管中"])[cn_ind];
+    this[1][i++]  = (["Putting felt pieces in crazy straw","将布片串在吸管中"])[cn_ind];
     this[0][i]    = "20200707_1";
     this[1][i++]  = (["Watching Ms Dave making roti dough","看Dave老师制作面团"])[cn_ind];
     this[0][i]    = "20200708_1";
