745b8ad018c2587d942adfc2a52cb5229ac4d93f
ymqui <ymqui@yahoo.com>
2020-10-02 11:36:10 -0400

    Add files via upload

--- info/info_2018goddard.js
+++ info/info_2018goddard.js
@@ -1051,6 +1051,8 @@ function initAlbum(){
     this[1][i++]  = (["Reading with friends","读书"])[cn_ind];
     this[0][i]    = "20201001_1";
     this[1][i++]  = (["Painting","画画"])[cn_ind];
+    this[0][i]    = "20201002_1";
+    this[1][i++]  = (["Practicing yoga","练习瑜伽"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
