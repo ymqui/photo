b222999a469ba9f26c1f24a5d2e9f15c5f31a25e
ymqui <ymqui@yahoo.com>
2020-10-31 18:32:29 -0400

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -190,7 +190,9 @@ function initAlbum(){
     this[1][i++]  = (["New toy","新玩具"])[cn_ind];
     this[0][i]    = ["2010fallfestival/2010fallfestival_7","20201030"];
     this[1][i++]  = (["With Mommy at the "+pagelink("2010fallfestival","Goddard School Fall Festival"),"和妈妈在"+pagelink("2010fallfestival","戈达德托儿所金秋节")+"上"])[cn_ind];
-     this[0][i]    = "20201031_018_BirthdayCake";
+    this[0][i]    = "20201031_014_BlackHillRP";
+    this[1][i++]  = (["With Mommy at the Black Hill Regional Park","和妈妈在黑山公园"])[cn_ind];
+    this[0][i]    = "20201031_018_BirthdayCake";
     this[1][i++]  = (["Happy 3rd Birthday!","三岁生日快乐！"])[cn_ind];
    
     for (var i=0;i<this[0].length;i++){
