d9643ed34296927477c8f783629e8501f216ef3b
ymqui <ymqui@yahoo.com>
2020-11-06 15:57:12 -0500

    Add files via upload

--- info/info_2020nist.js
+++ info/info_2020nist.js
@@ -27,6 +27,8 @@ function initAlbum(){
     this[1][i++]  = (["Making a turkey","贴火鸡"])[cn_ind];
     this[0][i]    = "20201104_2";
     this[1][i++]  = (["Making a turkey","贴火鸡"])[cn_ind];
+    this[0][i]    = "20201106_1";
+    this[1][i++]  = (["Painting with corn cob","用玉米画画"])[cn_ind];
      
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
