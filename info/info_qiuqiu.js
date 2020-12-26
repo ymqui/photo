3f5ee490e9d797ccf6510be9effa7789a08d52a9
ymqui <ymqui@yahoo.com>
2020-12-25 20:28:54 -0500

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -200,6 +200,8 @@ function initAlbum(){
     this[1][i++]  = (["First day to "+daycare,"第一天上"+daycare])[cn_ind];
     this[0][i]    = "20201122_021_Chopstick";
     this[1][i++]  = (["First time using chopsticks for dinner","第一次用筷子吃饭"])[cn_ind];
+    this[0][i]    = "20201225_022_ChristmasGift";
+    this[1][i++]  = (["Merry Christmas","圣诞快乐"])[cn_ind];
    
     for (var i=0;i<this[0].length;i++){
         if (this[0][i].constructor != Array){ 
