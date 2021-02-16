c4217c4b30a6fcf318bf1d4ef3fce93a1d4ce105
ymqui <ymqui@yahoo.com>
2021-02-16 18:33:41 -0500

    Add files via upload

--- info/info_2020nist.js
+++ info/info_2020nist.js
@@ -162,6 +162,8 @@ function initAlbum(){
     this[1][i++]  = (["Ms Sally is preparing for today's experiment","Sally老师在为今天的实验做准备"])[cn_ind];
     this[0][i]    = "20210211_1";
     this[1][i++]  = (["Lanterns decorating the hallway to celebrate the Chinese New Year","走廊挂满灯笼来庆祝农历新年"])[cn_ind];
+    this[0][i]    = "20210216_1";
+    this[1][i++]  = (["Lunch","午餐"])[cn_ind];
 
     
     for (var i=0;i<this[0].length;i++){
