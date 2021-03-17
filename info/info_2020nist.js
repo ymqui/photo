036c5705dd867c06c37d9cf5d85e9fcb8d8efd0b
ymqui <ymqui@yahoo.com>
2021-03-17 19:35:11 -0400

    Add files via upload

--- info/info_2020nist.js
+++ info/info_2020nist.js
@@ -215,7 +215,7 @@ function initAlbum(){
     this[0][i]    = "20210316_3";
     this[1][i++]  = (["Balancing a bean bag on a racket","在球拍上平衡豆袋"])[cn_ind];
     this[0][i]    = "20210317_1";
-    this[1][i++]  = (["Sticking gold coins on a pot","在坛子上贴金币"])[cn_ind];
+    this[1][i++]  = (["Making a pot of gold","做一坛金币"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
