7fe2fd49a10911e7cdc5d0b7b06d72cd3cc90f27
ymqui <ymqui@yahoo.com>
2021-05-24 06:14:28 -0400

    Add files via upload

--- info/info_2020nist.js
+++ info/info_2020nist.js
@@ -328,6 +328,12 @@ function initAlbum(){
     this[1][i++]  = (["Holding a live cicada","拿着一只活的知了"])[cn_ind];
     this[0][i]    = "20210520_1";
     this[1][i++]  = (["Outdoor PE class with Mr Palmer","Palmer老师的户外体育课"])[cn_ind];
+    this[0][i]    = "20210521_1";
+    this[1][i++]  = (["Morning music time with Ms Dinah","Dinah老师的早间音乐课"])[cn_ind];
+    this[0][i]    = "20210521_2";
+    this[1][i++]  = (["Leaving the work of cooking and childcare up to Molly and Philomena","将烹饪和育儿工作留给Molly和Philomena"])[cn_ind];
+    this[0][i]    = "20210521_3";
+    this[1][i++]  = (["Making a phone call","打电话"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
