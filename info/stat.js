b5a5b9d7b2874d8feba2ccc557487c37f5334e20
ymqui <ymqui@yahoo.com>
2021-07-22 17:24:49 -0400

    Add files via upload

--- info/stat.js
+++ info/stat.js
@@ -1,6 +1,6 @@
 <!--
   var blnk = 'https://www.worldbirdnames.org/new/updates/';
   var clnk = 'https://en.wikipedia.org/wiki/List_of_sovereign_states';
-  var btot = 10806;
+  var btot = 10912;
   var ctot = 195;
 //-->
