006fbfbf8be097b84d4bf6adbfd4fc7791004aa0
ymqui <ymqui@yahoo.com>
2021-07-26 20:33:55 -0400

    Add files via upload

--- info/info_2107trainride.js
+++ info/info_2107trainride.js
@@ -23,7 +23,7 @@ function initAlbum(){
     this.length = 2;
     this[0] = new Array();  //store file names
     this[1] = new Array();  //store file infos,shown as browser status
-    for (var i=0;i<14;i++){
+    for (var i=0;i<24;i++){
         this[0][i] = "2107trainride_"+(i+1).toString();
         this[1][i] = "";
     }
@@ -32,11 +32,21 @@ function initAlbum(){
         this[1][1] = "在蒸汽火车上。球球在来的路上流鼻血了。";
         this[1][8] = "跟火车司机挥手。";
         this[1][10] = "跟下一趟火车上的乘客挥手。";
+        this[1][14] = "等着坐马车。";
+        this[1][15] = "坐马车。";
+        this[1][17] = "在斯特拉斯堡火车站和James哥哥和候子阿姨会面好开心。";
+        this[1][18] = "在木头玩具火车里玩。";
+        this[1][20] = "坐蒸汽小火车。";
     }else{
         this[1][0] = "Brushing teeth after getting up.";
         this[1][1] = "On the steam train. Nathan had a nosebleed on the way to the Strasburg Rail Road.";
         this[1][8] = "Waving hands to the train engineer.";
         this[1][10] = "Waving hands to a passing train.";
+        this[1][14] = "Waiting for the horse carriage ride.";
+        this[1][15] = "Taking a horse carriage ride.";
+        this[1][17] = "So happy to meet James and Auntie Houzi at the Strasburg rail station.";
+        this[1][18] = "Playing in the playground.";
+        this[1][20] = "Taking a mini train ride.";
     }
 }
  
