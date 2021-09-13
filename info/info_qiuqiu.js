7c6627994f5b688ff73aa7305629d163a97039b0
ymqui <ymqui@yahoo.com>
2021-09-12 20:19:47 -0400

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -234,6 +234,8 @@ function initAlbum(){
     this[1][i++]  = (["With Mommy and Daddy on the deck at home","和爸爸妈妈在家里的阳台上"])[cn_ind];
     this[0][i]    = "20210828_YelenaCrabSalad";
     this[1][i++]  = (["Making crab salad with Auntie Yelena","和Yelena阿姨一起做螃蟹沙拉"])[cn_ind];
+    this[0][i]    = "20210911_Haircut";
+    this[1][i++]  = (["Getting a haircut from Mommy","妈妈给剪头发"])[cn_ind];
   
     for (var i=0;i<this[0].length;i++){
         if (this[0][i].constructor != Array){ 
