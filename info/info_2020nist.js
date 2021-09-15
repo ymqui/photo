8db21eaed8da195218ea8e525a8f5480404c6ced
ymqui <ymqui@yahoo.com>
2021-09-15 10:03:23 -0400

    Add files via upload

--- info/info_2020nist.js
+++ info/info_2020nist.js
@@ -404,7 +404,7 @@ function initAlbum(){
     this[0][i]    = "20210913_1";
     this[1][i++]  = (["Making a friendship bracelet for a friend","制作友谊手链送给朋友"])[cn_ind];
     this[0][i]    = "20210914_1";
-    this[1][i++]  = (["Practicing letters","练习写字母"])[cn_ind];
+    this[1][i++]  = (["Practice writing letters","练习写字母"])[cn_ind];
 
     for (var i=0;i<this[0].length;i++){
         this[1][i] = getdate(this[0][i],this[1][i]);
