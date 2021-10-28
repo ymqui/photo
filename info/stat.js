e074d50f0ebb911bf5937f88b0c143698cde6247
ymqui <ymqui@yahoo.com>
2021-10-28 11:37:54 -0400

    Add files via upload

--- info/stat.js
+++ info/stat.js
@@ -3,4 +3,7 @@
   var clnk = 'https://en.wikipedia.org/wiki/List_of_sovereign_states';
   var btot = 10912;
   var ctot = 195;
+  var is_chinese = /chinese/.test(window.location.search.substring(1));
+  var cn_ind = is_chinese?1:0;
+  document.title=(["LaoQ's Birding Stats","LaoQ的观鸟统计"])[cn_ind];
 //-->
