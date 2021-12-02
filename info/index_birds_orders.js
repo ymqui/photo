c4025ce9cf814b7b40a4cca57d584b0b25c51567
ymqui <ymqui@yahoo.com>
2021-12-02 18:42:24 -0500

    Add files via upload

--- info/index_birds_orders.js
+++ info/index_birds_orders.js
@@ -527,7 +527,7 @@
          tmp_cinfo[i]  = loclink(info[6*i],info[1+6*i],true,info[3+6*i],info[5+6*i]); 
      }
      for (var i=0;i<this.photo.length;i++){
-         if (this.photo[i].match(/_dig_/i)){dig_cnts++;}
+         if (this.photo[i].match(/_dig_/i)!=null){dig_cnts++;}
          this.info.push(tmp_info[Math.min(i,tmp_info.length-1)]); 
          this.cinfo.push(tmp_cinfo[Math.min(i,tmp_cinfo.length-1)]);
      }
