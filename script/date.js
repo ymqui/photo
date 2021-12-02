8fda74e9a0a024010dba481505bf40a0ad394587
ymqui <ymqui@yahoo.com>
2021-12-02 10:06:29 -0500

    Add files via upload

--- script/date.js
+++ script/date.js
@@ -1,13 +1,13 @@
-﻿<!--
-  var modiYear = modiDate.getYear();
-  modiYear = (modiYear<2000)?modiYear+1900:modiYear;
+<!--
+  var modYear = modDate.getYear();
+  modYear = (modYear<2000)?modYear+1900:modYear;
   var tmpdat = "";
   if(window.usechinese){
-     tmpdat = '最后更新: '+modiYear+'年'+(modiDate.getMonth()+1).toString()+'月'+modiDate.getUTCDate()+"日"; 
+     tmpdat = '最后更新: '+modYear+'年'+(modDate.getMonth()+1).toString()+'月'+modDate.getUTCDate()+"日"; 
   }else{
      var monthName = new Array("January","February","March","April","May",
 	    "June","July","August","September","October","November","December");
-     tmpdat = 'Last modified: '+monthName[modiDate.getMonth()]+" "+modiDate.getUTCDate()+", "+modiYear;
+     tmpdat = 'Last modified: '+monthName[modDate.getMonth()]+" "+modDate.getUTCDate()+", "+modYear;
   }
   document.write(tmpdat);
 //-->
