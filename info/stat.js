f3848200ca8bea152b77d1052e0271750d77a5d8
ymqui <ymqui@yahoo.com>
2021-12-23 13:11:43 -0500

    Add files via upload

--- info/stat.js
+++ info/stat.js
@@ -4,7 +4,7 @@
   var btot = 10912;
   var ctot = 195;
   var usechinese = /chinese/.test(window.location.search.substring(1));
-  var cn_ind = is_chinese?1:0;
+  var cn_ind = usechinese?1:0;
   var favico = (['stat2.ico','stat.ico'])[Math.round(Math.random())];
   document.title=(["LaoQ's Birding Stats","LaoQ的观鸟统计"])[cn_ind];
   document.write('<LINK rel="shortcut icon" href="https://ymqui.github.io/photo/thumbnail/'+favico+'">');
