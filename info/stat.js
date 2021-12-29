93caf31b80281dd5a9403b459b454b9443d294ba
ymqui <ymqui@yahoo.com>
2021-12-29 09:41:40 -0500

    Add files via upload

--- info/stat.js
+++ info/stat.js
@@ -3,6 +3,7 @@
   var clnk = 'https://en.wikipedia.org/wiki/List_of_sovereign_states';
   var btot = 10912;
   var ctot = 195;
+  var gtot = 2372;
   var usechinese = /chinese/.test(window.location.search.substring(1));
   var cn_ind = usechinese?1:0;
   var favico = (['stat2.ico','stat.ico'])[Math.round(Math.random())];
