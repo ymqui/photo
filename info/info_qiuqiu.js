a2e1a0aa3eb06ab96ee81feb96965adc8b25075d
ymqui <ymqui@yahoo.com>
2022-03-04 18:38:58 -0500

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -264,7 +264,7 @@ function initAlbum(){
     this[0][i]    = "20220127_002_MorningVideo";
     this[1][i++]  = (["Morning video time","早餐前看会儿视频"])[cn_ind];
     this[0][i]    = "20220304_003_Reading";
-    this[1][i++]  = (["Reading on the sofa","在沙发上看书"])[cn_ind];
+    this[1][i++]  = (["Reading on the sofa before dinner","晚饭前在沙发上看书"])[cn_ind];
 
     adddate(this);
 }
