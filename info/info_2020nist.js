cdd60e849a4ba6d4f1337c1629883fef505ed0c8
ymqui <ymqui@yahoo.com>
2022-06-09 06:43:55 -0400

    Add files via upload

--- info/info_2020nist.js
+++ info/info_2020nist.js
@@ -511,6 +511,8 @@ function initAlbum(){
     this[1][i++]  = (["Playing soccer","踢足球"])[cn_ind];
     this[0][i]    = "20220425_5";
     this[1][i++]  = (["Making a butterfly out of hand prints",制作手印蝴蝶"])[cn_ind];
+    this[0][i]    = "20220527_1";
+    this[1][i++]  = (["Spanish class","西班牙语课"])[cn_ind];
 
     adddate(this);
 }
