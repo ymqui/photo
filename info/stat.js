8ca3458843a7b3e7ef71788a52e7c37f15d80374
ymqui <ymqui@yahoo.com>
2022-08-17 09:46:58 -0400

    Add files via upload

--- info/stat.js
+++ info/stat.js
@@ -1,9 +1,9 @@
 <!--
   var blnk = 'https://www.worldbirdnames.org/new/updates/';
   var clnk = 'https://en.wikipedia.org/wiki/List_of_sovereign_states';
-  var btot = 10928;
+  var btot = 10933;
   var ctot = 195;
-  var gtot = 2376;
+  var gtot = 2384;
   var usechinese = /chinese/.test(window.location.search.substring(1));
   var cn_ind = usechinese?1:0;
   var favico = (['stat2.ico','stat.ico'])[Math.round(Math.random())];
