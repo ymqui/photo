bcf2c0d7488bbd1db29ebfa881098fbfa5b72b44
ymqui <ymqui@yahoo.com>
2022-08-31 20:26:17 -0400

    Add files via upload

---
+++ info/info_2208canada.js
@@ -0,0 +1,56 @@
+<!--
+//Created by Album Builder Program Wed Aug 31 19:42:05 2022
+var info_url   = "";
+var title      = "Trip to Canada";
+var cam_img    = "camera_canon_6d.jpg";
+var cam_tit    = "Canon SLR EOS 6D, 20.2 Mega Pixels.";
+var dir        = maindir+"pics/2208canada/";
+var thumb_dir  = maindir+"thumbnail/";
+var interval   = 5000;
+var ext        = ".jpg";
+var midi_file  = "";
+//var midi_cr    = new Array("","");
+var blocksave  = false;
+homeurl        = homeurl+"showphoto.html?qiuqiu";
+ 
+if(window.usechinese){
+    title      = "加拿大之行";
+    cam_tit    = "佳能单镜头反光式6D相机，2020万像素。";
+}
+ 
+document.title = title;
+ 
+function initAlbum(){
+    this.length = 2;
+    this[0] = new Array();  //store file names
+    this[1] = new Array();  //store file infos,shown as browser status
+    for (var i=0;i<9;i++){
+        this[0][i] = "2208canada_"+(i+1).toString();
+        this[1][i] = "";
+    }
+    if(window.usechinese){
+        this[1][0] = "在加拿大一边看尼亚加拉大瀑布。";
+        this[1][1] = "在加拿大一边看尼亚加拉大瀑布。";
+        this[1][2] = "在加拿大一边看尼亚加拉大瀑布。";
+        this[1][3] = "尼亚加拉大瀑布的双层公共汽车。";
+        this[1][4] = "安大略湖岸。";
+        this[1][5] = "在宾夕法尼亚州的凯恩小镇吃冰淇淋";
+        this[1][6] = "在宾夕法尼亚州的凯恩小镇吃冰淇淋";
+        this[1][7] = "宾夕法尼亚州凯恩小镇的向日葵花田。";
+    }else{
+        this[1][0] = "Niagara Falls, Canada.";
+        this[1][1] = "Niagara Falls, Canada.";
+        this[1][2] = "Niagara Falls, Canada.";
+        this[1][3] = "Niagara Falls, Canada.";
+        this[1][4] = "Hamilton beach.";
+        this[1][5] = "Country Pines Ice Cream Shop, Kane, Pennsylvania.";
+        this[1][6] = "Country Pines Ice Cream Shop, Kane, Pennsylvania.";
+        this[1][7] = "Sunflower fields in Kane, Pennsylvania.";
+    }
+}
+ 
+//web counter info
+var sc_project   = 9592725;
+var sc_invisible = 1;
+var sc_security  = "8084e75a";
+//-->
--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -22,6 +22,7 @@ var pagelist   = [["1806myrtlebeach","6/2018 Myrtle Beach, South Carolina","6/20
  	 	  ["2107philly","7/2021 Visiting Aunt Houzi in Pennsylvania","7/2021 宾夕法尼亚州候子阿姨家玩"],["2108vacation","8/2021 Pittsburgh and Provincetown Vacation","8/2021 匹兹堡和普罗温斯敦度假"],
                   ["2110longwood&showall","10/2021 Visiting Longwood Gardens in Pennsylvania","10/2021 宾夕法尼亚州游长木花园"],["2205florida","4/2022 Vacationing in Florida","4/2022 佛罗里达州度假"],
                   ["2206graduation","6/2022 Pre-K Graduation","6/2022 学前班毕业典礼"],["2208arizona","8/2022 Vacationing in Arizona","8/2022 亚利桑那州度假"],
+                  ["2208canada","8/2022 Trip to Canada","8/2022 加拿大之行"],
                   ["2020nist","11/2020-"+strDate+" NIST Child Care Center","11/2020-"+strDate+" NIST托儿所"]];
 var thisyear   = (new Date()).getFullYear();
 if (thisyear>2030) {pagelist = pagelist.concat([["university","2020-"+thisyear.toString()+" Visiting Universities","2020-"+thisyear.toString()+" 参观大学"]]);} 
@@ -284,9 +285,9 @@ function initAlbum(){
     this[1][i++]  = (["Bowling with James and Adele","小何叔叔带着去打保龄球"])[cn_ind];
     this[0][i]    = ["2208arizona/2208arizona_3","20220807"];
     this[1][i++]  = (["Visiting the Arizona-Sonora Desert Musuem in Tucson during the "+pagelink("2208arizona","Arizona vacation"),"在"+pagelink("2208arizona","亚利桑那州度假")+"时游亚利桑那-索诺拉沙漠博物馆"])[cn_ind];
-    this[0][i]    = "20220826_003_NiagaraFalls";
-    this[1][i++]  = (["Visiting the Niagara Falls in Canada","在加拿大一边看尼亚加拉大瀑布"])[cn_ind];
-    this[0][i]    = "20220828_012_PAKaneIceCreamShop";
+    this[0][i]    = ["2208canada/2208canada_2","20220826"];
+    this[1][i++]  = (["Visiting the Niagara Falls on the "+pagelink("2208canada","trip to Canada"),"在"+pagelink("2208canada","加拿大之行")+"看尼亚加拉大瀑布"])[cn_ind];
+    this[0][i]    = ["2208canada/2208canada_7","20220828"];
     this[1][i++]  = (["Enjoying a cup of ice cream at Country Pines Ice Cream Shop in Kane, Pennsylvania","在宾夕法尼亚州的凯恩小镇吃冰淇淋"])[cn_ind];
 
     adddate(this);
