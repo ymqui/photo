6b4883d98498a533057d6032fe1c820c6d112dcd
ymqui <ymqui@yahoo.com>
2022-09-04 07:31:25 -0400

    Add files via upload

--- info/info_world.js
+++ info/info_world.js
@@ -24,13 +24,13 @@ function initAlbum(){
     this[1] = new Array();  //store file infos,shown as browser status
     var i = 0;    
     this[0][i]    = "20181204_CostaRica";
-    this[1][i++]  = pagelink("1812costarica",(["Arenal Volcano, Alajuela Province, Costa Rica","哥斯达黎加阿拉胡埃拉省阿雷纳火山"])[cn_ind]);
+    this[1][i++]  = pagelink("1812costarica",(["Arenal Volcano, Alajuela Province, "+coltext("Costa Rica"),coltext("哥斯达黎加")+"阿拉胡埃拉省阿雷纳火山"])[cn_ind]);
     this[0][i]    = "20190420_USA";
-    this[1][i++]  = pagelink("1904dcmuseum",(["National Mall, Washington DC, United States","美国华盛顿特区国家广场"])[cn_ind]);
+    this[1][i++]  = pagelink("1904dcmuseum",(["National Mall, Washington DC, "+coltext("United States"),coltext("美国")+"华盛顿特区国家广场"])[cn_ind]);
     this[0][i]    = "20190511_Mexico";
-    this[1][i++]  = pagelink("1905mexico",(["Teotihucan Pyramid of the Sun, Mexico","墨西哥特奥蒂瓦坎古城太阳金字塔"])[cn_ind]);
+    this[1][i++]  = pagelink("1905mexico",(["Teotihucan Pyramid of the Sun, "+coltext("Mexico"),coltext("墨西哥")+"特奥蒂瓦坎古城太阳金字塔"])[cn_ind]);
     this[0][i]    = "20220826_Canada";
-    this[1][i++]  = pagelink("2208canada",(["Niagara Falls, Ontario, Canada","加拿大安大略省尼亚加拉大瀑布"])[cn_ind]);
+    this[1][i++]  = pagelink("2208canada",(["Niagara Falls, Ontario, "+coltext("Canada"),coltext("加拿大")+"安大略省尼亚加拉大瀑布"])[cn_ind]);
 
     adddate(this);
 }
