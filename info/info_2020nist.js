deaa7e8bba5979264ae722bffc434e625d905940
ymqui <ymqui@yahoo.com>
2022-10-03 19:22:57 -0400

    Add files via upload

--- info/info_2020nist.js
+++ info/info_2020nist.js
@@ -523,6 +523,8 @@ function initAlbum(){
     this[1][i++]  = (["Painting with Victor","和同学Victor一块画画"])[cn_ind];
     this[0][i]    = "20220930_1";
     this[1][i++]  = (["Painting with Bella","和同学Bella一块画画"])[cn_ind];
+    this[0][i]    = "20221003_1";
+    this[1][i++]  = (["Reading with Issac and Hao Cheng","和同学Issac和程浩一块看书"])[cn_ind];
 
     adddate(this);
 }
