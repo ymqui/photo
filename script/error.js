ca69030c6cd77d0587d720c0807ee17ab55150c5
ymqui <ymqui@yahoo.com>
2023-04-07 07:27:52 -0400

    Add files via upload

--- script/error.js
+++ script/error.js
@@ -1,12 +1,8 @@
 ﻿<!--
     var googledrive = (window.location.href.search("github")!=-1);
     var cellphone = (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i));
-    if(window.usechinese){
-        homeurl = "https://ymqui.github.io/zhaopian/";
-    }else{
-        homeurl = "https://ymqui.github.io/photo/";  
-    }
-    var maindir = "https://ymqui.github.io/photo/";
+    var homeurl = (window.usechinese)?"https://ymqui.github.io/zhaopian/":"https://ymqui.github.io/photo/";
+    var maindir = ;
     //window.onerror = function(msg,url,line)
     //{   
     //    window.location.href = homeurl+"error.html?"+msg+' at line '+line;
