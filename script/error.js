4ee14e212a7bf01b9264b4c5ac6ca41a8c1e9dfb
ymqui <ymqui@yahoo.com>
2023-04-07 10:24:25 -0400

    Add files via upload

--- script/error.js
+++ script/error.js
@@ -1,8 +1,8 @@
-﻿<!--
-    var googledrive = (window.location.href.search("github")!=-1);
+<!--
+    //var googledrive = (window.location.href.search("github")!=-1);
     var cellphone = (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i));
     var homeurl = (window.usechinese)?"https://ymqui.github.io/zhaopian/":"https://ymqui.github.io/photo/";
-    var maindir = ;
+    var maindir = "https://ymqui.github.io/photo/";
     //window.onerror = function(msg,url,line)
     //{   
     //    window.location.href = homeurl+"error.html?"+msg+' at line '+line;
