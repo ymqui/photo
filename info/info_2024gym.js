9226e3533fcb9b6e281ab6cc37806cb45e4527c8
ymqui <ymqui@yahoo.com>
2024-07-08 09:24:43 -0400

    Add files via upload

---
+++ info/info_2024gym.js
@@ -0,0 +1,33 @@
+<!--
+var info_url   = "https://www.thelittlegym.com/maryland-gaithersburg/";
+var title      = (["Nathan at Little Gym","球球在Little Gym学习体操"])[cn_ind];
+var dir        = maindir+"pics/2024gym/";
+var thumb_dir  = maindir+"thumbnail/";
+var interval   = 5000;
+var ext        = ".jpg";
+var midi_file  = "StoryOfMyLife.mid";
+var midi_cr    = new Array("Piano It All","http://www.pianoitall.com");
+var last2first = true;
+var makeface   = new Array();
+var url        = "";
+homeurl        = homeurl+"showphoto.html?qiuqiu";
+ 
+document.title = title;
+ 
+function initAlbum(){
+    this.length = 2;
+    this[0] = new Array();  //store file names
+    this[1] = new Array();  //store file infos,shown as browser status
+    i = 0;
+    j = 0;
+    this[0][i]    = ["20240707_1","20240707_2","20240707_3"];
+    this[1][i++]  = ([urllink((url="https://youtu.be/HwFekbYxxrY?t=189"),"Trying out at the Little Gym (YouTube video)"),urllink(url,"在Little Gym尝试练习体操(YouTube视频)")])[cn_ind];
+
+    adddate(this);
+}
+
+//web counter info
+var sc_project   = 9592725;
+var sc_invisible = 1;
+var sc_security  = "8084e75a";
+//-->
