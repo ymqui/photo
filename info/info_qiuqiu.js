677fb8e5f59a1baecb311036f53febe6af161ae7
ymqui <ymqui@yahoo.com>
2024-11-01 19:16:28 -0400

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -407,6 +407,8 @@ function initAlbum(){
     this[1][i++]  = (["Making a Rainbow Loom triple single bracelet","用橡皮筋编三重手链"])[cn_ind];
     this[0][i]    = "20241026_033_WithGrandparents";
     this[1][i++]  = (["So happy to see Grandpa and Grandma again, especially with so many toys","见到外公外婆好开心，特别是又带来好多玩具"])[cn_ind];
+    this[0][i]    = "20241031_035_Birthday";
+    this[1][i++]  = (["Happy 7th birthday!","七岁生日快乐！"])[cn_ind];
 
     if (/birthday/.test(window.location.search.substring(1))) {
        var n_id = 0;
