3c510c36721498a3aaa827e030420e69afe7b9d0
ymqui <ymqui@yahoo.com>
2025-01-03 18:56:24 -0500

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -427,7 +427,7 @@ function initAlbum(){
     this[0][i]    = ["2412florida/2412florida_15","2412florida/2412florida_19","20241229"];
     this[1][i++]  = (["Visiting Hemingway's Key West home in heavy rain during "+pagelink("2412florida","the road trip to Florida"),pagelink("2412florida","在佛罗里达度假")+"时冒着大雨参观海明威在西礁岛的故居"])[cn_ind];
     this[0][i]    = ["2412florida/2412florida_14","20241230"];
-    this[1][i++]  = (["Losing the first tooth","掉了第一颗牙齿"])[cn_ind];
+    this[1][i++]  = (["Losing the first tooth during "+pagelink("2412florida","the road trip to Florida"),pagelink("2412florida","在佛罗里达度假")+"时掉了第一颗牙齿"])[cn_ind];
     this[0][i]    = ["2412florida/2412florida_24","2412florida/2412florida_25","20241230"];
     this[1][i++]  = (["Visiting the river front of Savannah with Grandpa and Grandma during "+pagelink("2412florida","the road trip to Florida"),pagelink("2412florida","在佛罗里达度假")+"时和爷爷奶奶一起逛萨凡纳的河滨街道"])[cn_ind];
 
