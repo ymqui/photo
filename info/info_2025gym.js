98baf04a037760186b28f2f46c3901187917d066
ymqui <ymqui@yahoo.com>
2025-06-08 15:48:46 -0400

    Add files via upload

---
+++ info/info_2025gym.js
@@ -0,0 +1,33 @@
+<!--
+var info_url   = "https://www.dominiquedawesgymnasticsacademy.com/";
+var title      = (["Nathan at Dominique Dawes Gymnastics & Ninja Academy","球球在Dominique Dawes体操馆学体操"])[cn_ind];
+var dir        = maindir+"pics/2025gym/";
+var thumb_dir  = maindir+"thumbnail/";
+var interval   = 5000;
+var ext        = ".jpg";
+var midi_file  = "StoryOfMyLife.mid";
+var midi_cr    = new Array("Piano It All","http://www.pianoitall.com");
+var last2first = true;
+var makeface   = new Array();
+var url        = "";
+homeurl        = homeurl+"showphoto.html?qiuqiu";
+ 
+document.title = title;
+ 
+function initAlbum(){
+    this.length = 2;
+    this[0] = new Array();  //store file names
+    this[1] = new Array();  //store file infos,shown as browser status
+    i = 0;
+    j = 0;
+    this[0][i]    = "20250606";
+    this[1][i++]  = (["Arriving at the gym","到达体操馆"])[cn_ind];
+
+    adddate(this);
+}
+
+//web counter info
+var sc_project   = 9592725;
+var sc_invisible = 1;
+var sc_security  = "8084e75a";
+//-->
