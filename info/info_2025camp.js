fcdbcff9d056979d466de06da6fd9a8d5a5666f4
ymqui <ymqui@yahoo.com>
2025-06-18 11:43:54 -0400

    Add files via upload

---
+++ info/info_2025camp.js
@@ -0,0 +1,33 @@
+<!--
+var info_url   = "https://www.visartscenter.org/";
+var title      = (["2025 Summer Camp","2025夏令营"])[cn_ind];
+var dir        = maindir+"pics/2025camp/";
+var thumb_dir  = maindir+"thumbnail/";
+var interval   = 5000;
+var ext        = ".jpg";
+var midi_file  = "StoryOfMyLife.mid";
+var midi_cr    = new Array("Piano It All","http://www.pianoitall.com");
+var last2first = true;
+var makeface   = new Array();
+var url        = "";
+homeurl        = homeurl+"showphoto.html?qiuqiu";
+ 
+document.title = title;
+ 
+function initAlbum(){
+    this.length = 2;
+    this[0] = new Array();  //store file names
+    this[1] = new Array();  //store file infos,shown as browser status
+    i = 0;
+    j = 0;
+    this[0][i]    = "20250618_1";
+    this[1][i++]  = (["Woodworking at VisArts summer camp","VisArts夏令营的木工课"])[cn_ind];
+
+    adddate(this);
+}
+
+//web counter info
+var sc_project   = 9592725;
+var sc_invisible = 1;
+var sc_security  = "8084e75a";
+//-->
--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -35,7 +35,7 @@ var pagelist   = [["1806myrtlebeach","6/2018 Myrtle Beach, South Carolina","6/20
                   ["2412florida","12/2024 Vacationing in Florida","12/2024 佛罗里达州度假"],["2503liberty","3/2025 Statute of Liberty","3/2025 参观自由女神像"],
                   ["2504guatemala","4/2025 Spring Break in Guatemala","4/2025 在危地马拉度春假"],["2505williamsburg","5/2025 Colonial Williamsburg","5/2025 威廉斯堡"],
                   ["2024gym","7/2024-6/2025 Little Gym","7/2024-6/2025 Little Gym小体操馆"],["2025gym","5/2025-"+strDate+" Dominique Dawes Gymnastics","5/2025-"+strDate+" Dominique Dawes体操馆"],
-                  ["2023fallsmead","8/2023-"+strDate+" Fallsmead Elementary School","8/2023-"+strDate+" Fallsmead小学"]];
+                  ["2025camp","6/2025-"+strDate+"2025 Summer Camp","6/2025-"+strDate+" 2025夏令营"],["2023fallsmead","8/2023-"+strDate+" Fallsmead Elementary School","8/2023-"+strDate+" Fallsmead小学"]];
 if (thisyear>2030) {pagelist = pagelist.concat([["university","2020-"+thisyear.toString()+" Visiting Universities","2020-"+thisyear.toString()+" 参观大学"]]);} 
 if (thisyear>2026) {pagelist = pagelist.concat([["world","2018-"+thisyear.toString()+" World Travel","2018-"+thisyear.toString()+" 环游世界"]]);} 
 
