8e5944b680241c40f952c2c0e9c6073f995c4b61
ymqui <ymqui@yahoo.com>
2025-08-25 09:25:05 -0400

    Add files via upload

--- info/info_2025gym.js
+++ info/info_2025gym.js
@@ -20,6 +20,7 @@ function initAlbum(){
     this[1] = new Array();  //store file infos,shown as browser status
     i = 0;
     j = 0;
+    tmptxt = (["Taking gymnastics class at the Gym","在体操馆学体操"])[cn_ind];
     this[0][i]    = "20250606";
     this[1][i++]  = (["Arriving at the gym","到达体操馆"])[cn_ind];
     this[0][i]    = ["20250613_1","20250613_2"];
@@ -29,9 +30,11 @@ function initAlbum(){
     this[0][i]    = ["20250628_1","20250628_2"];
     this[1][i++]  = ([urllink((url="https://youtu.be/lB__J5EJzFA"),"Taking gymnastics class at the Gym (YouTube video)"),urllink(url,"在体操馆学体操(YouTube视频)")])[cn_ind];
     this[0][i]    = ["20250712_1","20250712_2","20250712_3","20250712_4"];
-    this[1][i++]  = (["Taking gymnastics class at the Gym","在体操馆学体操"])[cn_ind];
+    this[1][i++]  = tmptxt;
     this[0][i]    = ["20250726_1","20250726_2"];
-    this[1][i++]  = (["Taking gymnastics class at the Gym","在体操馆学体操"])[cn_ind];
+    this[1][i++]  = tmptxt;
+    this[0][i]    = "20250823_1";
+    this[1][i++]  = tmptxt;
 
     adddate(this);
 }
