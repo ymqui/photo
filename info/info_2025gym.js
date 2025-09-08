63550d383363f297f671c56d4f54e3dc5e3f9a1a
ymqui <ymqui@yahoo.com>
2025-09-08 09:17:24 -0400

    Add files via upload

--- info/info_2025gym.js
+++ info/info_2025gym.js
@@ -20,21 +20,24 @@ function initAlbum(){
     this[1] = new Array();  //store file infos,shown as browser status
     i = 0;
     j = 0;
-    tmptxt = (["Taking gymnastics class at the Gym","在体操馆学体操"])[cn_ind];
+    let tmptxt1   = (["Taking gymnastics class at the Gym","在体操馆学体操"])[cn_ind];
+    let tmptxt2   = (["Taking gymnastics class at the Gym (YouTube video)","在体操馆学体操(YouTube视频)"])[cn_ind];
     this[0][i]    = "20250606";
     this[1][i++]  = (["Arriving at the gym","到达体操馆"])[cn_ind];
     this[0][i]    = ["20250613_1","20250613_2"];
-    this[1][i++]  = (["Practicing at the gym","在体操馆练习"])[cn_ind];
+    this[1][i++]  = tmptxt1;
     this[0][i]    = "20250621_1";
-    this[1][i++]  = ([urllink((url="https://youtube.com/shorts/3g9RT4JcxDo?feature=share"),"Taking gymnastics class at the Gym (YouTube video)"),urllink(url,"在体操馆学体操(YouTube视频)")])[cn_ind];
+    this[1][i++]  = urllink((url="https://youtube.com/shorts/3g9RT4JcxDo?feature=share"),tmptxt2);
     this[0][i]    = ["20250628_1","20250628_2"];
-    this[1][i++]  = ([urllink((url="https://youtu.be/lB__J5EJzFA"),"Taking gymnastics class at the Gym (YouTube video)"),urllink(url,"在体操馆学体操(YouTube视频)")])[cn_ind];
+    this[1][i++]  = urllink((url="https://youtu.be/lB__J5EJzFA"),tmptxt2);
     this[0][i]    = ["20250712_1","20250712_2","20250712_3","20250712_4"];
-    this[1][i++]  = tmptxt;
+    this[1][i++]  = tmptxt1;
     this[0][i]    = ["20250726_1","20250726_2"];
-    this[1][i++]  = tmptxt;
+    this[1][i++]  = tmptxt1;
     this[0][i]    = "20250823_1";
-    this[1][i++]  = tmptxt;
+    this[1][i++]  = tmptxt1;
+    this[0][i]    = ["20250906_1","20250906_2"];
+    this[1][i++]  = urllink((url="https://youtube.com/shorts/Ouk40Vkw8Sc?feature=share"),tmptxt2);
 
     adddate(this);
 }
