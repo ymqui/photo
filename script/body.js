0f1dc9a2442e167a9fa81f123b287a3a0639130c
ymqui <ymqui@yahoo.com>
2025-09-19 11:24:40 -0400

    Add files via upload

--- script/body.js
+++ script/body.js
@@ -67,58 +67,32 @@
          }
      }
   }
-  if(cellphone) document.write('<br>');
-  var pagelistid = 1;
 
-  if(window.usechinese){
-     pagelistid = 2;
-     document.write('<A onclick="getFirst()"><IMG SRC="'+thumb_dir+prefix+'first.png"'+borderstr('第一张')+'></A>');
-     document.write('<A onclick="getPrev()"><IMG SRC="'+thumb_dir+prefix+'previous.png"'+borderstr('前一张或点击照片左侧')+'></A>');
-     document.write('<A onclick="getNext()"><IMG SRC="'+thumb_dir+prefix+'next.png"'+borderstr('下一张或点击照片右侧')+'></A>');
-     document.write('<A onclick="getLast()"><IMG SRC="'+thumb_dir+prefix+'last.png"'+borderstr('最后一张')+'></A>');
-     document.write('<IMG SRC="'+thumb_dir+prefix+'vbar.png" style="margin-bottom:2px">');
-     document.write('<A onclick="startstopShow()"><IMG name="slideimg" id="slideimg" SRC="'+thumb_dir+prefix+'slideshow.png"'+borderstr('自动放映/按F11全屏显示')+'></A>');
-     if(use_mid){
-        document.write('<A onclick="startstopMidi()"><IMG name="midiimg" id="midiimg" SRC="'+thumb_dir+prefix+'midi.png" width="0" height="0"'+borderstr('播放音乐')+'></A>');
-     }
-     document.write('<A onclick="refit()"><IMG name="refitimg" id="refitimg" SRC="'+thumb_dir+prefix+'size_actual.png"'+borderstr('照片本来大小')+'></A>');
-     document.write('<IMG SRC="'+thumb_dir+prefix+'vbar.png" style="margin-bottom:2px">');
-     if(use_cam){
-        document.write('<A onclick="getCamera()"><IMG SRC="'+thumb_dir+prefix+'camera.png"'+borderstr('照相机')+'></A>');
-     }
-     if(use_url){
-        document.write('<A onclick="moreInfo()"><IMG SRC="'+thumb_dir+prefix+'info.png"'+borderstr('其它信息')+'></A>');
-     }
-     document.write('<A onclick="switchlang()"><IMG SRC="'+thumb_dir+prefix+'english_v.png"'+borderstr('英文版/English Version')+'></A>');
-     document.write('<A onclick="backhome(homeurl)"><IMG SRC="'+thumb_dir+prefix+'home.png"'+borderstr('相册主页')+'></A>');
-     if(typeof addbday !== "undefined"){
-        document.write('<A onclick="switchbirthday()"><IMG name="bdayimg" SRC="'+thumb_dir+prefix+'birthday.png"'+borderstr('生日照片')+'></A>');
-     }
-  }else{
-     document.write('<A onclick="getFirst()"><IMG SRC="'+thumb_dir+prefix+'first.png"'+borderstr('First')+'></A>');
-     document.write('<A onclick="getPrev()"><IMG SRC="'+thumb_dir+prefix+'previous.png"'+borderstr('Previous or Click on the Left Side of the Photo')+'></A>');
-     document.write('<A onclick="getNext()"><IMG SRC="'+thumb_dir+prefix+'next.png"'+borderstr('Next or Click on the Right Side of the Photo')+'></A>');
-     document.write('<A onclick="getLast()"><IMG SRC="'+thumb_dir+prefix+'last.png"'+borderstr('Last')+'></A>');
-     document.write('<IMG SRC="'+thumb_dir+prefix+'vbar.png" style="margin-bottom:2px">');
-     document.write('<A onclick="startstopShow()"><IMG name="slideimg" id="slideimg" SRC="'+thumb_dir+prefix+'slideshow.png"'+borderstr('Start Slide Show/Press F11 Full Screen')+'></A>');
-     if(use_mid){
-        document.write('<A onclick="startstopMidi()"><IMG name="midiimg" id="midiimg" SRC="'+thumb_dir+prefix+'midi.png" width="0" height="0"'+borderstr('Play Music')+'></A>');
-     }
-     document.write('<A onclick="refit()"><IMG name="refitimg" id="refitimg" SRC="'+thumb_dir+prefix+'size_actual.png"'+borderstr('Actual Photo Size')+'></A>');
-     document.write('<IMG SRC="'+thumb_dir+prefix+'vbar.png" style="margin-bottom:2px">');
-     if(use_cam){
-        document.write('<A onclick="getCamera()"><IMG SRC="'+thumb_dir+prefix+'camera.png"'+borderstr('Camera')+'></A>');
-     }
-     if(use_url){
-        document.write('<A onclick="moreInfo()"><IMG SRC="'+thumb_dir+prefix+'info.png"'+borderstr('More Info')+'></A>');
-     }
-     document.write('<A onclick="switchlang()"><IMG SRC="'+thumb_dir+prefix+'chinese_v.png"'+borderstr('Chinese Version/中文版')+'></A>');
-     document.write('<A onclick="backhome(homeurl)"><IMG SRC="'+thumb_dir+prefix+'home.png"'+borderstr('Photo Album Home')+'></A>');
-     if(typeof addbday !== "undefined"){
-        document.write('<A onclick="switchbirthday()"><IMG name="bdayimg" SRC="'+thumb_dir+prefix+'birthday.png"'+borderstr('Birthday Photos')+'></A>');
-     }
+  if(cellphone) document.write('<br>');
+  var pagelistid = ([1,2])[cn_ind];
+  document.write('<A onclick="getFirst()"><IMG SRC="'+thumb_dir+prefix+'first.png"'+borderstr((['First','第一张'])[cn_ind])+'></A>');
+  document.write('<A onclick="getPrev()"><IMG SRC="'+thumb_dir+prefix+'previous.png"'+borderstr((['Previous or Click on the Left Side of the Photo','前一张或点击照片左侧'])[cn_ind])+'></A>');
+  document.write('<A onclick="getNext()"><IMG SRC="'+thumb_dir+prefix+'next.png"'+borderstr((['Next or Click on the Right Side of the Photo','下一张或点击照片右侧'])[cn_ind])+'></A>');
+  document.write('<A onclick="getLast()"><IMG SRC="'+thumb_dir+prefix+'last.png"'+borderstr((['Last','最后一张'])[cn_ind])+'></A>');
+  document.write('<IMG SRC="'+thumb_dir+prefix+'vbar.png" style="margin-bottom:2px">');
+  document.write('<A onclick="startstopShow()"><IMG name="slideimg" id="slideimg" SRC="'+thumb_dir+prefix+'slideshow.png"'+borderstr((['Start Slide Show/Press F11 Full Screen','自动放映/按F11全屏显示'])[cn_ind])+'></A>');
+  if(use_mid){
+     document.write('<A onclick="startstopMidi()"><IMG name="midiimg" id="midiimg" SRC="'+thumb_dir+prefix+'midi.png" width="0" height="0"'+borderstr((['Play Music','播放音乐'])[cn_ind])+'></A>');
   }
-  
+  document.write('<A onclick="refit()"><IMG name="refitimg" id="refitimg" SRC="'+thumb_dir+prefix+'size_actual.png"'+borderstr((['Actual Photo Size','照片本来大小'])[cn_ind])+'></A>');
+  document.write('<IMG SRC="'+thumb_dir+prefix+'vbar.png" style="margin-bottom:2px">');
+  if(use_cam){
+     document.write('<A onclick="getCamera()"><IMG SRC="'+thumb_dir+prefix+'camera.png"'+borderstr((['Camera','照相机'])[cn_ind])+'></A>');
+  }
+  if(use_url){
+     document.write('<A onclick="moreInfo()"><IMG SRC="'+thumb_dir+prefix+'info.png"'+borderstr((['More Info','其它信息'])[cn_ind])+'></A>');
+  }
+  document.write('<A onclick="switchlang()"><IMG SRC="'+thumb_dir+prefix+'english_v.png"'+borderstr((['Chinese Version/中文版','英文版/English Version'])[cn_ind])+'></A>');
+  document.write('<A onclick="backhome(homeurl)"><IMG SRC="'+thumb_dir+prefix+'home.png"'+borderstr((['Photo Album Home','相册主页'])[cn_ind])+'></A>');
+  if(typeof addbday !== "undefined"){
+     document.write('<A onclick="switchbirthday()"><IMG name="bdayimg" SRC="'+thumb_dir+prefix+'birthday.png"'+borderstr((['Birthday Photos','生日照片'])[cn_ind])+'></A>');
+  }
+
   var click = ' onclick="mouseclick(event)"';
   if(window.blocksave && (!cellphone)){click = click+' oncontextmenu="rightclick(event)"';}
   document.write('<p>&nbsp;');
@@ -138,11 +112,7 @@
      for (var i=pagelist.length-1;i>=0;i--){
          document.write(pagelink(pagelist[i][0],pagelist[i][pagelistid]));
          if (!cellphone){
-            if (i!=0){
-               document.write('<font color='+link_color+'>,</font>');
-            }else{
-               document.write('<font color='+link_color+'>.</font>');
-            }
+            document.write('<font color='+link_color+'>'+((i==0)?'.':',')+'</font>');
          }
          document.write('&nbsp;');
          if (i%3==0||cellphone){document.write('<br>');}else{document.write('&nbsp;&nbsp;');}
@@ -158,22 +128,13 @@
 
   //credit the source of the midi file
   if(use_cr){
-     if(window.sc_project){
-         document.write('<div id="midicr"> </div>');
-     }else{
-         document.write('<p>&nbsp;<p><div id="midicr"> </div>');
-     }
+     if(typeof sc_project === 'undefined') document.write('<p>&nbsp;<p>');
+     document.write('<div id="midicr"> </div>');
+     cr_str = midi_cr[0];
      if(midi_cr.length==2){
-         cr_str = '<A style="color:'+color_4+'" target="newWin" href="'+midi_cr[1]+'">'+midi_cr[0]+'</A>';
-     }else{
-         cr_str = midi_cr[0];    
+         cr_str = '<A style="color:'+color_4+'" target="newWin" href="'+midi_cr[1]+'">'+cr_str+'</A>';
      }
-     if(window.usechinese){
-         cr_str = 'MIDI文件由 '+cr_str+' 提供。';
-     }else{
-         cr_str = 'The MIDI file is from '+cr_str+'.';
-     }    
-     cr_str = '<font color='+color_4+'>&copy; '+cr_str+'</font>';
+     cr_str = '<font color='+color_4+'>&copy; '+(['The MIDI file is from ','MIDI文件由 '])[cn_ind]+cr_str+(['.',' 提供。'])[cn_ind]+'</font>';
   }
 
   if(use_mid){
