ca30eb8963568347c81a3af313078b95f316523f
ymqui <ymqui@yahoo.com>
2025-10-21 06:54:26 -0400

    Add files via upload

--- info/info_2510blackwater.js
+++ info/info_2510blackwater.js
@@ -29,9 +29,9 @@ function initAlbum(){
         this[1][i] = "";
     }
     if(window.usechinese){
-        this[1][0] = "在黑水瀑布山道上的“几乎是天堂”秋千上荡秋千。";
+        this[1][0] = "球球和妈妈在黑水瀑布山道上的“几乎是天堂”秋千上荡秋千。";
         this[1][1] = "在黑水瀑布。";
-        this[1][2] = "在大石头上做劈叉。";
+        this[1][2] = "球球在大石头上做劈叉。";
         this[1][3] = "走埃拉卡拉山道前往埃拉卡拉瀑布。";
         this[1][4] = "埃拉卡拉山道。";
         this[1][5] = "埃拉卡拉山道。";
@@ -39,14 +39,14 @@ function initAlbum(){
         this[1][7] = "埃拉卡拉瀑布。";
         this[1][8] = "埃拉卡拉瀑布。";
         this[1][9] = "埃拉卡拉瀑布。";
-        this[1][10] = "展示体操技巧。";
+        this[1][10] = "球球展示体操技巧。";
         this[1][11] = "在林迪角，可以欣赏到黑水峡谷的壮丽景色。";
         this[1][12] = "在林迪角。";
         this[1][13] = "在林迪角。";
     }else{
-        this[1][0] = "Swinging on the Almost Heaven swing on the Blackwater Falls Trail.";
+        this[1][0] = "Nathan and Mom swinging on the Almost Heaven swing on the Blackwater Falls Trail.";
         this[1][1] = "At the Blackwater Falls.";
-        this[1][2] = "Doing a split on the giant boulder.";
+        this[1][2] = "Nathan doing a split on the giant boulder.";
         this[1][3] = "Hiking the Elakala Trail.";
         this[1][4] = "Hiking the Elakala Trail.";
         this[1][5] = "Hiking the Elakala Trail.";
@@ -54,7 +54,7 @@ function initAlbum(){
         this[1][7] = "At the Elakala Falls.";
         this[1][8] = "The Elakala Falls.";
         this[1][9] = "The Elakala Falls.";
-        this[1][10] = "Showing off his gymnastics skill.";
+        this[1][10] = "Nathan showing off his gymnastics skill.";
         this[1][11] = "At the Lindy Point, with a gorgeous view of the Blackwater Canyon.";
         this[1][12] = "At the Lindy Point.";
         this[1][13] = "At the Lindy Point.";
