ff44b1e4a95edbcaca250a7b6b6de9e86e111ca9
ymqui <ymqui@yahoo.com>
2025-11-02 06:46:35 -0500

    Add files via upload

---
+++ info/info_2510tot.js
@@ -0,0 +1,50 @@
+<!--
+//Created by Album Builder Program Sun Nov 02 06:32:44 2025
+var info_url   = "";
+var title      = "Trick-or-treating";
+var cam_img    = "camera_canon_6d.jpg";
+var cam_tit    = "Canon SLR EOS 6D, 20.2 Mega Pixels.";
+var dir        = maindir+"pics/2510tot/";
+var thumb_dir  = maindir+"thumbnail/";
+var interval   = 5000;
+var ext        = ".jpg";
+var midi_file  = "";
+//var midi_cr    = new Array("","");
+var blocksave  = false;
+homeurl        = homeurl+"showphoto.html?qiuqiu";
+ 
+if(window.usechinese){
+    title      = "万圣节索糖";
+    cam_tit    = "佳能单镜头反光式6D相机，2020万像素。";
+}
+ 
+document.title = title;
+ 
+function initAlbum(){
+    this.length = 2;
+    this[0] = new Array();  //store file names
+    this[1] = new Array();  //store file infos,shown as browser status
+    for (var i=0;i<18;i++){
+        this[0][i] = "2510tot_"+(i+1).toString();
+        this[1][i] = "";
+    }
+    if(window.usechinese){
+        this[1][0] = "球球和朋友Bella与妈妈一起出门去要糖。";
+        this[1][14] = "清点万圣节糖果。";
+        this[1][15] = '喝“蓝色汤”。';
+        this[1][16] = '喝“蓝色汤”。';
+        this[1][17] = "玩相机。";
+    }else{
+        this[1][0] = "Nathan and Bella setting out for trick-or-treating with parents.";
+        this[1][14] = "Sorting Halloween candy.";
+        this[1][15] = 'Drinking "blue soup".';
+        this[1][16] = 'Drinking "blue soup".'";
+        this[1][17] = "Having fun with the camera.";
+    }
+}
+ 
+//web counter info
+var sc_project   = 9592725;
+var sc_invisible = 1;
+var sc_security  = "8084e75a";
+//-->
--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -35,7 +35,7 @@ var pagelist   = [["1806myrtlebeach","6/2018 Myrtle Beach, South Carolina","6/20
                   ["2504guatemala","4/2025 Spring Break in Guatemala","4/2025 在危地马拉度春假"],["2505williamsburg","5/2025 Colonial Williamsburg","5/2025 威廉斯堡"],
                   ["2024gym","7/2024-6/2025 Little Gym","7/2024-6/2025 Little Gym小体操馆"],["2507plymouth","7/2025 Plymouth Whale Watch","7/2025 普利茅斯观鲸"],
                   ["2025camp","6/2025-8/2025 Summer Camp","6/2025-8/2025 夏令营"],["2508italy","8/2025 Vacationing in Italy","8/2025 在意大利度假"],
-                  ["2510blackwater","10/2025 Blackwater Falls State Park","10/2025 黑水瀑布州立公园"],
+                  ["2510blackwater","10/2025 Blackwater Falls State Park","10/2025 黑水瀑布州立公园"],["2510tot","10/2025 Trick-or-Treating","10/2025 万圣节索糖"],
                   ["2025gym","5/2025-"+strDate+" Dominique Dawes Gymnastics","5/2025-"+strDate+" 多米尼克·道斯体操馆"],["2023fallsmead","8/2023-"+strDate+" Fallsmead Elementary School","8/2023-"+strDate+" Fallsmead小学"]];
 if (thisyear>2032) {pagelist.push(["university","2020-"+thisyear.toString()+" Visiting Universities","2020-"+thisyear.toString()+" 参观大学"]);} 
 if (thisyear>2030) {pagelist.push(["world","2018-"+thisyear.toString()+" World Travel","2018-"+thisyear.toString()+" 环游世界"]); } 
@@ -490,6 +490,8 @@ function initAlbum(){
     addbday.push(i); 
     this[0][i]   = ["20251031_021_Birthday","20251031_023_Birthday"];
     this[1][i++] = (["Happy 8th birthday!","八岁生日快乐！"])[cn_ind];
+    this[0][i]   = ["2510tot/2510tot_1","2510tot/2510tot_4","2510tot/2510tot_9","20251031"];
+    this[1][i++] = pagelink("2510tot",(["Trick-or-treating with Bella","万圣节和和朋友Bella一起去要糖"])[cn_ind]);
 
     adddate(this);
 }
