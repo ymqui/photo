f2259613325a10b146f2f123f44083a0ae3f8391
ymqui <ymqui@yahoo.com>
2026-01-20 16:38:38 -0500

    Add files via upload

--- info/index_birds_orders.js
+++ info/index_birds_orders.js
@@ -86,9 +86,9 @@ var pt_eng_chn = [["males* left","左雄性"],["males* right","右雄性"],["fem
     ["nonbreeding females*\/immature males*","非繁殖羽雌性/未成年雄性"],["(females*, *nonbreeding plumage|nonbreeding females*)","非繁殖羽雌性"],["winter plumage","冬羽"],["1st winter","一龄冬羽"],["2nd winter","二龄冬羽"],
     ["2nd year","二龄羽"],["3rd winter","三龄冬羽"],["3rd year","三龄羽"],["winter females*","冬羽雌性"],["eclipse males*","蚀羽雄性"],["molting adult","换羽成年鸟"],["molting (immatures*|juveniles*)","换羽未成年鸟"],
     ["adult feeding young","成年鸟喂雏鸟"],["adults*","成年"],["adults* and (chicks*|hatchlings*)","成年和幼鸟"],["adults* and (immatures*|juveniles*)","成年和未成年"],["breeding (adults*|plumage)","繁殖羽"],
-    ["nonbreeding *(adult)*\/immature","非繁殖羽/未成年"],["nests*","鸟巢"],["nonbreeding (adults*|plumage)","非繁殖羽"],["(partially)* *leucistic","白变种"],["mating display","求偶展示"],["light morph","浅色型"],
-    ["(immature|juvenile) light morph","未成年浅色型"],["dark morph","深色型"],["(immature|juvenile) dark morph","未成年深色型"],["(chicks*|hatchlings*)","幼鸟"],["males* at nest cavity","雄性在鸟巢口"],
-    ["females* at nest cavity","雌性在鸟巢口"],["(on the)* *left","左边"],["(on the)* *right","右边"],["common","普通型"],["slate-colored form","石板色型"],["red morph","红色型"]];
+    ["nonbreeding *(adult)*\/immature","非繁殖羽/未成年"],["nests*","鸟巢"],["nonbreeding (adults*|plumage)","非繁殖羽"],["(partially)* *leucistic","白变种"],["mating display","求偶展示"],["light (morph|form)","浅色型"],
+    ["dark (morph|form)","深色型"],["common","普通型"],["gray (morph|form)","灰色型"],["red (morph|form)","红色型"],["slate-colored form","石板色型"],["white (morph|form)","白色型"],["(chicks*|hatchlings*)","幼鸟"],
+    ["males* at nest cavity","雄性在鸟巢口"],["females* at nest cavity","雌性在鸟巢口"],["(on the)* *left","左边"],["(on the)* *right","右边"]];
 var fam_ln  = 8;  //family length
 var order   = (/&order|^order/i).test(window.location.search.substring(1));
 var comma   = [", ","，"];
