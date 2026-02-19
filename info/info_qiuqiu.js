121a50b46a7422d77572798af107646ea9eed32c
ymqui <ymqui@yahoo.com>
2026-02-19 12:43:19 -0500

    Add files via upload

--- info/info_qiuqiu.js
+++ info/info_qiuqiu.js
@@ -151,7 +151,6 @@ function initAlbum(){
     this[1][i++] = (["New toy","新玩具"])[cn_ind];
     this[0][i]   = "20181007_048_StandingWithMom";
     this[1][i++] = (["Standing with Mommy's help","妈妈扶着站起来"])[cn_ind];
-    addbday.push(i);
     this[0][i]   = "20181031_051_WithMom";
     this[1][i++] = (["Happy 1st birthday!","一岁生日快乐！"])[cn_ind];
     this[0][i]   = ["1810parade/1810parade_3","20181031"];
@@ -190,7 +189,6 @@ function initAlbum(){
     this[1][i++] = (["Junior firefighter","小小消防员"])[cn_ind];
     this[0][i]   = ["1910parade/1910parade_10","20191031"];
     this[1][i++] = pagelink("1910parade",(["Goddard School Halloween parade","戈达德托儿所万圣节游行"])[cn_ind]);
-    addbday.push(i);
     this[0][i]   = "20191031_023";
     this[1][i++] = (["Happy 2nd birthday!","两岁生日快乐！"])[cn_ind];
     this[0][i]   = "20191225_061";
@@ -217,7 +215,6 @@ function initAlbum(){
     this[1][i++] = (["With Mommy at the "+pagelink("2010fallfestival","Goddard School Fall Festival"),"和妈妈在"+pagelink("2010fallfestival","戈达德托儿所金秋节")+"上"])[cn_ind];
     this[0][i]   = "20201031_014_BlackHillRP";
     this[1][i++] = (["With Mommy at the Black Hill Regional Park","和妈妈在黑山公园"])[cn_ind];
-    addbday.push(i);
     this[0][i]   = "20201031_018_BirthdayCake";
     this[1][i++] = (["Happy 3rd birthday!","三岁生日快乐！"])[cn_ind];
     school       = pagelink("2020nist",(["the NIST Child Care Center","NIST托儿所"])[cn_ind]);
@@ -263,7 +260,6 @@ function initAlbum(){
     this[1][i++] = (["Waiting to get on the school bus at "+school+" for a field trip to the Butler's Orchard","在"+school+"排队上校车去巴特勒果园"])[cn_ind];
     this[0][i]   = ["2110longwood/2110longwood_10","20211009"];
     this[1][i++] = ([pagelink("2110longwood&showall","Visiting Longwood Gardens")+" in Pennsylvania","在宾夕法尼亚州"+pagelink("2110longwood&showall","游览长木花园")])[cn_ind];
-    addbday.push(i);
     this[0][i]   = "20211031_021_WithMommy";
     this[1][i++] = (["Happy 4th birthday!","四岁生日快乐！"])[cn_ind];
     this[0][i]   = ["20211031_024_TrickorTreat","20211031_025_TrickorTreat","20211031_026_TrickorTreat"];
@@ -315,7 +311,6 @@ function initAlbum(){
     this[1][i++] = (["Picking apples at the Homestead Farm in Poolesville","在普尔斯维尔的Homestead农场摘苹果"])[cn_ind];
     this[0][i]   = ["2210farm/2210farm_1","2210farm/2210farm_13","20221020"];
     this[1][i++] = ([school+" "+pagelink("2210farm","field trip to Summers Farm"),"球球上的"+school+"大班去"+pagelink("2210farm","Summers农场秋游")])[cn_ind];
-    addbday.push(i);
     this[0][i]   = "20221031_012_Birthday";
     this[1][i++] = (["Happy 5th birthday!","五岁生日快乐！"])[cn_ind];
     makeface.push(i);
@@ -358,7 +353,6 @@ function initAlbum(){
     this[1][i++] = (["Clay animals made by Nathan","球球捏的小动物"])[cn_ind];
     this[0][i]   = ["2310parade/2310parade_9","20231031"];
     this[1][i++] = pagelink("2310parade",(["Fallsmead Elementary School Halloween parade","Fallsmead小学万圣节游行"])[cn_ind]);
-    addbday.push(i);
     this[0][i]   = ["20231031_065_Birthday","20231031_067_Birthday"];
     this[1][i++] = (["Happy 6th birthday!","六岁生日快乐！"])[cn_ind];
     this[0][i]   = ["20231105_070_RakingLeaves","20231105_073_RakingLeaves"];
@@ -415,7 +409,6 @@ function initAlbum(){
     this[1][i++] = (["Making a Rainbow Loom triple single bracelet","用橡皮筋编三重手链"])[cn_ind];
     this[0][i]   = "20241026_033_WithGrandparents";
     this[1][i++] = (["So happy to see Grandpa and Grandma again, especially with so many toys","见到外公外婆好开心，特别是又带来好多玩具"])[cn_ind];
-    addbday.push(i); 
     this[0][i]   = "20241031_035_Birthday";
     this[1][i++] = (["Happy 7th birthday!","七岁生日快乐！"])[cn_ind];
     this[0][i]   = ["2411niagara/2411niagara_4","2411niagara/2411niagara_8","20241103"];
@@ -488,7 +481,6 @@ function initAlbum(){
     this[1][i++] = ([["Practicing on the home-made balance beam","Practicing on the high bar"],["在自制的平衡木上练习","在单杠上练习"]])[cn_ind];
     this[0][i]   = ["2510blackwater/2510blackwater_2","2510blackwater/2510blackwater_13","20251019"];
     this[1][i++] = pagelink("2510blackwater",(["Visiting the Blackwater Falls State Park in West Virginia","参观西弗吉尼亚州的黑水瀑布州立公园"])[cn_ind]);
-    addbday.push(i); 
     this[0][i]   = ["20251031_021_Birthday","20251031_023_Birthday"];
     this[1][i++] = (["Happy 8th birthday!","八岁生日快乐！"])[cn_ind];
     this[0][i]   = ["2510tot/2510tot_1","2510tot/2510tot_4","2510tot/2510tot_9","20251031"];
@@ -511,6 +503,7 @@ function initAlbum(){
     this[0][i]   = "20260217_Swim";
     this[1][i++] = (["Taking swimming lesson at Goldfish Swimming School","在金鱼游泳学校上游泳课"])[cn_ind];
 
+    addbday = this[1].reduce((result,ele,ind)=>{if(/(birthday!|生日快乐！)/i.test(ele))result.push(ind); return result;},[])
     adddate(this);
 }
 
