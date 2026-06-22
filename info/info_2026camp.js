76fff723b1430b3641c96dd7e6c7d517b65fc079
ymqui <ymqui@yahoo.com>
2026-06-22 16:35:00 -0400

    Add files via upload

---
+++ info/info_2026camp.js
@@ -0,0 +1,32 @@
+<!--
+var info_url   = "https://kidsafterhours.com/explorers/";
+var title      = (["2026 Summer Camp","2026夏令营"])[cn_ind];
+var dir        = maindir+"pics/2026camp/";
+var thumb_dir  = maindir+"thumbnail/";
+var interval   = 5000;
+var ext        = ".jpg";
+var midi_file  = "StoryOfMyLife.mid";
+var midi_cr    = new Array("Piano It All","http://www.pianoitall.com");
+var last2first = true;
+var makeface   = new Array();
+var url        = "";
+homeurl        = homeurl+"showphoto.html?qiuqiu";
+ 
+document.title = title;
+ 
+function initAlbum(){
+    this.length = 2;
+    this[0] = new Array();  //store file names
+    this[1] = new Array();  //store file infos,shown as browser status
+    i = 0;
+    this[0][i]    = "20250618_1";
+    this[1][i++]  = (["Woodworking at VisArts summer camp","VisArts夏令营的木工课"])[cn_ind];
+
+    adddate(this);
+}
+
+//web counter info
+var sc_project   = 9592725;
+var sc_invisible = 1;
+var sc_security  = "8084e75a";
+//-->
