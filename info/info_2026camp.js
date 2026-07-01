6b6f92a2955cfad88ad7f1f9e8c2e9d1d2882aec
ymqui <ymqui@yahoo.com>
2026-07-01 15:34:17 -0400

    Add files via upload

--- info/info_2026camp.js
+++ info/info_2026camp.js
@@ -39,6 +39,8 @@ function initAlbum(){
     this[1][i++]  = (["Visiting the Smithonian's National Air and Space Museum Udvar-Hazy Center in Chantilly, Virginia","参观位于弗吉尼亚州尚蒂利的国家航空航天博物馆乌德沃尔-哈齐中心"])[cn_ind];
     this[0][i]    = "20260629_1";
     this[1][i++]  = (["Visiting the Smithonian's National Air and Space Museum Udvar-Hazy Center in Chantilly, Virginia","参观位于弗吉尼亚州尚蒂利的国家航空航天博物馆乌德沃尔-哈齐中心"])[cn_ind];
+    this[0][i]    = "20260701_1";
+    this[1][i++]  = (["Visiting the Gunpowder Falls State Park in Middle River, Maryland","参观马里兰州的火药瀑布州立公园"])[cn_ind];
 
     adddate(this);
 }
