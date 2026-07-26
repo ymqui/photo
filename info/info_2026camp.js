35c6b4be7ffe328dcfde5bcf2de58f314bf9188e
ymqui <ymqui@yahoo.com>
2026-07-25 20:15:40 -0400

    Add files via upload

--- info/info_2026camp.js
+++ info/info_2026camp.js
@@ -87,12 +87,12 @@ function initAlbum(){
     this[1][i++]  = (["Visiting the Great Falls Park in Potomac, Maryland","游马里兰州一侧的大瀑布公园"])[cn_ind];
     this[0][i]    = "20260720_3";
     this[1][i++]  = (["Visiting the Great Falls Park in Potomac, Maryland","游马里兰州一侧的大瀑布公园"])[cn_ind];
+    this[0][i]    = "20260722_0";
+    this[1][i++]  = (["Decorating the bus","装饰校车"])[cn_ind];
     this[0][i]    = "20260722_1";
     this[1][i++]  = (["Bus ride","乘车的途中"])[cn_ind];
     this[0][i]    = "20260722_2";
     this[1][i++]  = (["Painting a giant panda at the canvas paint party","画一只大熊猫"])[cn_ind];
-//    this[0][i]    = "20260723_1";
-//    this[1][i++]  = (["River tubing","河流漂流"])[cn_ind];
     this[0][i]    = "20260724_1";
     this[1][i++]  = (["Visiting the Wonderfly Arena in Arbutus, Maryland","在马里兰州阿布图斯的神奇飞蝇游乐场(Wonderfly Arena)玩"])[cn_ind];
     this[0][i]    = "20260724_2";
