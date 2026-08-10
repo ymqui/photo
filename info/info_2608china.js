502c2b23345a123b5b0c4c0f33dbf12741b90a29
ymqui <ymqui@yahoo.com>
2026-08-10 12:37:03 -0400

    Add files via upload

---
+++ info/info_2608china.js
@@ -0,0 +1,49 @@
+<!--
+var info_url   = "";
+var title      = (["Trip to China","中国之行"])[cn_ind];
+var dir        = maindir+"pics/2608china/";
+var thumb_dir  = maindir+"thumbnail/";
+var interval   = 5000;
+var ext        = ".jpg";
+var midi_file  = "StoryOfMyLife.mid";
+var midi_cr    = new Array("Piano It All","http://www.pianoitall.com");
+var makeface   = new Array();
+var url        = "";
+homeurl        = homeurl+"showphoto.html?qiuqiu";
+ 
+document.title = title;
+ 
+function initAlbum(){
+    this.length = 2;
+    this[0] = new Array();  //store file names
+    this[1] = new Array();  //store file infos,shown as browser status
+    i = 0;
+    this[0][i]    = "20260804_001_IAD";
+    this[1][i++]  = (["Waiting at Washington Dulles International Airport for the flight to Seoul","在华盛顿杜勒斯国际机场等候飞往首尔的航班"])[cn_ind];
+    this[0][i]    = "20260805_002_ICN";
+    this[1][i++]  = (["Having dinner at Incheon International Airport in Korea","在韩国仁川国际机场吃晚餐"])[cn_ind];
+    this[0][i]    = "20260805_003_ICN";
+    this[1][i++]  = (["Having dinner at Incheon International Airport in Korea","在韩国仁川国际机场吃晚餐"])[cn_ind];
+    this[0][i]    = "20260805_004_ICN";
+    this[1][i++]  = (["Having dinner at Incheon International Airport in Korea","在韩国仁川国际机场吃晚餐"])[cn_ind];
+    this[0][i]    = "20260806_005_ICN";
+    this[1][i++]  = (["Eating breakfast at Incheon International Airport in Korea","在韩国仁川国际机场吃早餐"])[cn_ind];
+    this[0][i]    = ["20260809_006_Longyan","20260809_007_Longyan"];
+    this[1][i++]  = (["Playing a claw machine in Longyan, Fujian","在福建龙岩玩抓娃娃机"])[cn_ind];
+    this[0][i]    = ["20260810_008_Jiangshan","20260810_009_Jiangshan"];
+    this[1][i++]  = (["Enjoying Mei Cai Kou Rou (steamed pork belly with preserved mustard greens) cooked by Grandpa in Jiangshan, Zhejiang","在浙江江山吃外公做的梅菜扣肉"])[cn_ind];
+    this[0][i]    = "20260810_010_Jiangshan";
+    this[1][i++]  = (["Meeting Mommy's middle school teacher in Jiangshan, Zhejiang","在浙江江山和妈妈的初中老师见面"])[cn_ind];
+    this[0][i]    = "20260810_011_Jiangshan";
+    this[1][i++]  = (["Meeting Mommy's middle school classmates in Jiangshan, Zhejiang","在浙江江山和妈妈的初中同学见面"])[cn_ind];
+    this[0][i]    = "20260810_012_Jiangshan";
+    this[1][i++]  = (["At Grandparents' house in Jiangshan, Zhejiang","在浙江江山的外公外婆家"])[cn_ind];
+
+    adddate(this);
+}
+
+//web counter info
+var sc_project   = 9592725;
+var sc_invisible = 1;
+var sc_security  = "8084e75a";
+//-->
